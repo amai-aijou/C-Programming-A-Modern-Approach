98950a52e2f4e9bb2ee9fbe746af65ca482f1f39
amai-aijou <joseph.russ.olson@gmail.com>
2025-12-28 22:19:29 -0600

    Exercise 8-9

---
+++ c-a-modern-approach/eight/exercises/8-8.c
@@ -0,0 +1,15 @@
+/******************************************
+ * Name: 8-8.c
+ * Purpose: 
+ * Author: amai-aijou
+ * Date: Sun Dec 28 06:35:30 PM CST 2025
+ ******************************************/
+
+#include <stdio.h>
+
+int main(void) {
+
+	const int  temperature_readings[24][30];
+
+}
+
