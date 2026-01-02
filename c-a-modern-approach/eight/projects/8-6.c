569d112555c76a94865fe6223295fbdbf59ab3be
amai-aijou <joseph.russ.olson@gmail.com>
2026-01-02 16:30:50 -0600

    8-6...really this time

--- c-a-modern-approach/eight/projects/8-6.c
+++ c-a-modern-approach/eight/projects/8-6.c
@@ -6,34 +6,53 @@
  ******************************************/
 
 #include <stdio.h>
-#define N 40
+#include <ctype.h>
+
+#define N 60
 #define SIZE ((int) (sizeof(array) / sizeof(array[0])))
 
 int main(void) {
 	char ch;
 	char array[N] = {0};
+	int i = 0;
 
 	printf("Enter message: ");
-	
+/*	
 	for (i = 0; i < SIZE; i++) {
-		scanf("%c", &Array[i]);
+		scanf("%c", &array[i]);
+	}
+*/
+	while ((ch = getchar()) != '\n') {
+		array[i] = ch;
+		i++;
 	}
 
 	printf("In Biffspeak: ");
 	
 	for (i = 0; i < SIZE; i++) {
 
+		
 		switch (toupper(array[i])) {
 			case 'A': printf("4");
+					  break;
 			case 'B': printf("8");
+					  break;
 			case 'E': printf("3");
+					  break;
 			case 'I': printf("1");
+					  break;
 			case 'O': printf("0");
+					  break;
 			case 'S': printf("5");
-			default: printf("%c", array[i]);
+					  break;
+			case '\n': break;
+			default: printf("%c", toupper(array[i]));
+					 break;
 		}
 		
 	}
+
+	printf("!!!!!!!!!!\n");
 
 return 0;
 
