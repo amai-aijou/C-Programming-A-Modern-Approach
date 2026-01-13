6a23ad745d9e87422f4448df17cef63787370f8f
amai-aijou <joseph.russ.olson@gmail.com>
2026-01-12 21:29:54 -0600

    Chapter 9 notes

---
+++ c-a-modern-approach/nine/prime.c
@@ -0,0 +1,38 @@
+/******************************************
+ * Name: prime.c
+ * Purpose: 
+ * Author: amai-aijou
+ * Date: Mon Jan 12 08:04:18 PM CST 2026
+ ******************************************/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_prime(int n) {
+	int divisor;
+
+	if (n <= 1) {
+		return false;
+	}
+
+	for (divisor = 2; divisor * divisor <= n; divisor++) {
+		if (n % divisor == 0) {
+			return false;
+			}
+		return true;
+	}
+}
+
+int main(void) {
+	int n;
+
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	if (is_prime(n)) {
+		printf("Prime\n");
+	} else {
+		printf("Not Prime\n");
+	}
+
+	return 0;
+}
