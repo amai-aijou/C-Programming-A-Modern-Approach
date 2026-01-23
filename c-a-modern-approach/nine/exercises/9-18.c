f3d22968569734b9cc4fcb9e6fc667e1d548eb1d
amai-aijou <joseph.russ.olson@gmail.com>
2026-01-22 19:21:40 -0600

    9-18 exercise

--- c-a-modern-approach/nine/exercises/9-18.c
+++ c-a-modern-approach/nine/exercises/9-18.c
@@ -1,5 +1,5 @@
 /******************************************
- * Name: 9-3.c
+ * Name: 9-18.c
  * Purpose: 
  * Author: amai-aijou
  * Date: Thu Jan 15 08:25:43 PM CST 2026
@@ -8,6 +8,8 @@
 #include <stdio.h>
 
 int gcd(int m, int n);
+
+int gcd_new(int m, int n);
 
 int main(void) {
 	int m, n, x;
@@ -22,6 +24,7 @@ int main(void) {
 	}
 
 	printf("The GCD is: %d\n", gcd(m,n));
+	printf("The GCD (new) is: %d\n", gcd_new(m,n));
 
 	return 0;
 
@@ -41,3 +44,16 @@ int gcd(int m, int n) {
 
 }
 
+int gcd_new(int m, int n) {
+
+	int remainder = 0;
+
+	if (n == 0) {
+		return m;
+	} else {
+		remainder = m % n;
+		m = n;
+		n = remainder;
+		gcd(n,(m%n));
+	}
+}
