dde2203a9923ccdf123cfd254d686f26f33141cd
amai-aijou <joseph.russ.olson@gmail.com>
2026-02-17 16:46:49 -0600

    11-6

---
+++ c-a-modern-approach/eleven/exercises/11-6.c
@@ -0,0 +1,71 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name:  
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Mon Feb 16 07:14:03 PM CST 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+void find_two_largest(int a[], int n, int *largest, int *second_largest);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	int n = 5;
+	int first = 0;
+	int second = 0;
+	int i;
+	char ch;
+
+	printf("Please enter %d numbers: ", n);
+
+	int a[n];
+
+		for (i = 0; i < n; i++) {
+
+			scanf("%d", &a[i]);
+		}
+
+	for (i = 0; i < n; i++) {
+		printf("a[%d]:%d\n", i, a[i]);
+	}
+
+	printf("(Pre-function call) Largest:%d | Second Largest:%d\n", first, second);
+	
+	find_two_largest(a, n, &first, &second);
+
+	printf("Largest:%d | Second Largest:%d\n", first, second);
+
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+void find_two_largest(int a[], int n, int *largest, int *second_largest) {
+
+	int i;
+
+	for (i = 0; i < n; i++) {
+		if (a[i] > *largest) {
+			*largest = a[i];	
+		}
+	}
+
+	for (i = 0; i < n; i++) {
+		if (a[i] > *second_largest && a[i] < *largest) {
+			*second_largest = a[i];
+		}
+	}
+
+}
