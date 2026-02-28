25c411313bbd8ed4c9bb1e936f8a908bd8fb7d88
amai-aijou <joseph.russ.olson@gmail.com>
2026-02-27 18:28:47 -0600

    12-5 exercise

---
+++ c-a-modern-approach/twelve/exercises/12-5.c
@@ -0,0 +1,55 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name:  
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Fri Feb 27 06:10:13 PM CST 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	int a[10];
+	int *p;
+
+	p = a;
+
+	/* A should be illegal, can't compare pointer to integer
+	if (p == a[0]) {
+		printf("A is legal and true");
+	} */
+	printf("A is illegal, cannot compare pointer to integer.\n");
+
+	// B should be legal and true, "p = a" is equivalent to "p = a[0]"
+	if (p == &a[0]) {
+		printf("B is legal and true\n");
+	}
+
+	// C should be legal and true, as *p points to a[0]
+	if (*p == a[0]) {
+		printf("C is legal and true\n");
+	}
+
+	// D should be legal, p[0] is equivalent to "*p + 0", and a[0] is equivalent to "a + 0"
+	if (p[0] == a[0]) {
+		printf("D is legal and true\n");
+	}
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
