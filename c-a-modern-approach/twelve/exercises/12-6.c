9e2fa601d2664122478bad11297ed6fbb0ec5b57
amai-aijou <joseph.russ.olson@gmail.com>
2026-02-27 20:15:46 -0600

    12-6 exercises

---
+++ c-a-modern-approach/twelve/exercises/12-6.c
@@ -0,0 +1,69 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name:  
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Fri Feb 27 06:29:18 PM CST 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+// Global Variables
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int sum_array_subscript(const int a[*], int n);
+int sum_array_arithmetic(const int a[*], int n);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	int n = 5;
+	int a[5] = {2, 4, 6, 8, 10};
+
+	printf("Sum should be 30!\n\n");
+	printf("Array Subscripting: %d\n", sum_array_subscript(a, n));
+	printf("Pointer Arithmetic: %d\n", sum_array_arithmetic(a, n));
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int sum_array_subscript(const int a[], int n) {
+
+	int i, sum;
+
+	sum = 0;
+	for (i = 0; i < n; i++) {
+		sum += a[i];
+	}
+
+	return sum;
+}
+
+int sum_array_arithmetic(const int a[], int n) {
+
+	int sum;
+	const int *p;
+
+	sum = 0;
+
+	for (p = &a[0]; p < &a[n]; p++) {
+		sum += *p;
+	}
+
+	return sum;
+}
