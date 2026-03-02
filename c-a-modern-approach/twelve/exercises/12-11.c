a49ffc7068dd432d202a747989e6f0bfd4340804
amai-aijou <joseph.russ.olson@gmail.com>
2026-03-01 18:24:24 -0600

    12-11 exercise

--- c-a-modern-approach/twelve/exercises/12-11.c
+++ c-a-modern-approach/twelve/exercises/12-11.c
@@ -6,6 +6,8 @@
    ❤︎︎࣪ Date: Sun Mar  1 04:14:58 PM CST 2026
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
                                                                 
+//12-11: Modify find_largest function of 11.5 so it uses pointer arithmetic to calculate return value
+
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                       ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
@@ -15,21 +17,51 @@
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                   ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-find_largest
+int find_largest(int a[], int n); 
+int find_largest_arith(int *a, int n); 
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                 ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 int main(void) {
 
-	//12-11: Modify find_largest function of 11.5 so it uses pointer arithmetic to calculate return value
+	int n = 5;
+	int a[5] = {2, 4, 6, 8, 10};
 
+	printf("find_largest(): %d\n", find_largest(a, n));
+	printf("find_largest_arith(): %d\n", find_largest_arith(a, n));
+
+	return 0;
 }
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                    ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-int example_function(int i) {
+int find_largest(int a[], int n) {
 
+	int i, max;
+
+	max = a[0];
+	for (i = 1; i < n; i++) {
+		if (a[i] > max) {
+			max = a[i];
+		}
+	}
+
+	return max;
 }
 
+int find_largest_arith(int *a, int n) {
+
+	int *p;
+	int max = 0;
+
+	for (p = a + 1; p < a + n; p++) {
+		
+		if (*p > max) {
+			max = *p;
+		}
+	}
+
+	return max;
+}
