4430bbce1e31594a7378b6f853e21f7cbb2ff90b
amai-aijou <joseph.russ.olson@gmail.com>
2026-03-02 21:09:04 -0600

    12-14 exercise

---
+++ c-a-modern-approach/twelve/exercises/12-14.c
@@ -0,0 +1,83 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 12-14.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Mon Mar  2 08:13:19 PM CST 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+bool search(int a[*][24], int n, int key);
+void initialize_array(int temperatures[*][24]);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	int n = 7 * 24;
+	int temperatures[7][24];
+	int key = 0;
+
+	initialize_array(temperatures);
+
+	printf("Please enter a key: ");
+	scanf("%d", &key);
+
+	if (search(temperatures, n, key) == true) {
+		printf("Term %d was found\n", key);
+	} else {
+		printf("not found\n");
+	}
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+bool search(int a[][24], int n, int key) {
+
+	int *p;
+
+	for (p = a[0]; p < (a[0] + n); p++) {
+		if (*p == key) {
+			return true;
+		}
+	}
+
+	// default return (no match)
+	return false;
+}
+
+void initialize_array(int temperatures[][24]) {
+
+	int *p;
+	int a = 0;
+
+	for (p = *temperatures; p < *temperatures + (7 * 24); p++) {
+
+		if (a % 11 == 0) {
+			*p = 1;		
+		} else {
+			*p = 0;
+		}
+
+		if (a == 1) {
+			*p = 32;
+		}
+		a++;
+	}
+}
