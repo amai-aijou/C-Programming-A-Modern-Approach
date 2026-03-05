285a8bc05d8f343d06176564e4fc5c103cb9c54d
amai-aijou <joseph.russ.olson@gmail.com>
2026-03-04 21:09:09 -0600

    12-17 exercise

---
+++ c-a-modern-approach/twelve/exercises/12-17.c
@@ -0,0 +1,64 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 12-17.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Wed Mar  4 07:49:12 PM CST 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define LEN 5
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int sum_two_dimensional_array_subscript(const int a[*][LEN], int n);
+int sum_two_dimensional_array_arithmetic(const int a[*][LEN], int n);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	int n = 3 * LEN;
+	int a[3][LEN] = {{1, 2, 3, 4, 5},
+			  		 {1, 3, 5, 7, 9},
+					 {0, 2, 4, 6, 8}};
+
+	printf("sum (subscript): %d\n", sum_two_dimensional_array_subscript(a, 3));
+	printf("sum (arith): %d\n", sum_two_dimensional_array_arithmetic(a, n));
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int sum_two_dimensional_array_subscript(const int a[][LEN], int n) {
+
+	int i, j, sum = 0;
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < LEN; j++) {
+			sum += a[i][j];
+		}
+	}
+
+	return sum;
+}
+
+int sum_two_dimensional_array_arithmetic(const int a[][LEN], int n) {
+
+	const int *p;
+	int sum = 0;
+
+	for (p = a[0]; p < (a[0] + n); p++) {
+		sum += *p;
+	}
+
+	return sum;
+}
