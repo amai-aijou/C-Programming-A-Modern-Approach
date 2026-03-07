998fc49dc5399d94b330471b02f4ca1e3649247b
amai-aijou <joseph.russ.olson@gmail.com>
2026-03-06 22:17:14 -0600

    12-3, 12-4 Projects

---
+++ c-a-modern-approach/twelve/projects/12-4.c
@@ -0,0 +1,86 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 12-4.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Thu Mar  5 08:24:49 PM CST 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define MAX 100
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+void read_user_input_arith(char a[*], int *n);
+bool check_palindrome(char a[*], int *n);
+void print_array(char a[*], int *n);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	int n = 0;
+	char a[MAX];
+
+	read_user_input_arith(a, &n);
+
+	if (check_palindrome(a, &n)) {
+		printf("Palindrome.\n");
+	} else {
+		printf("Not a palindrome.\n");
+	}
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+void read_user_input_arith(char a[], int *n) {
+
+	char *p = a;
+	char ch;
+
+	printf("Enter a message: ");
+	while ((ch = getchar()) != '\n') {
+		if (isalpha(ch)) {
+			*(p + (*n)++) = tolower(ch);	
+		}
+	}
+}
+
+bool check_palindrome(char a[], int *n) {
+
+	char *p = a;
+	char ch;
+	int x = 0;
+
+	for (p = a; p <= a + (*n/2); p++) {
+
+		if (*p != *(p + (*n-1) - x)) {	
+			return false;
+		}
+		x += 2;
+	}
+
+	return true;
+}
+
+void print_array(char a[], int *n) {
+
+	char *p;
+
+	for (p = a; p < a + *n; p++) {
+		printf("%c ", *p);
+	}
+	printf("\n");
+}
