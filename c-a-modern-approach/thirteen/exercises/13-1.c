3ad43047581d0d3822f5316e0c13ff6ebaed3fa4
amai-aijou <joseph.russ.olson@gmail.com>
2026-03-13 22:38:37 -0500

    thirteen study and exercise 13-1

---
+++ c-a-modern-approach/thirteen/exercises/13-1.c
@@ -0,0 +1,57 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 13-1.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Fri Mar 13 10:20:40 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int example_function(int i);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	printf("----------\n");
+//	printf("%c", '\n');	//(a)
+	printf("%c", "\n"); //(b)
+//	printf("%s", '\n'); //(c)
+//	printf("%s", "\n"); //(d)
+//	printf('\n');		//(e)
+//	printf("\n");		//(f)
+//	putchar('\n');		//(g)
+//	putchar("\n");		//(h)
+//	puts('\n');			//(i)
+//	puts("\n");			//(j)
+//	puts("");			//(k)
+	printf("----------\n\n\n");
+
+	printf("My guesses:\n");
+	printf("(a) should print singular newline character (a blank line)\n");   
+	printf("(b) won't work, expecting string\n");   
+	printf("(c) won't work, expecting string\n");   
+	printf("(d) should create a string with one newline char and one null char\n");   
+	printf("(e) won't print, as printf requires a string\n");   
+	printf("(f) will print a newline character using printf\n");   
+	printf("(g) will place newline char\n");   
+	printf("(h) putchar expects a single char, not a string (even if it's one letter)\n");   
+	printf("(i) may or may not work\n");   
+	printf("(j) should print newline char\n");   
+	printf("(k) might print nothing?\n");   
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
