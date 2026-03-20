69dcf280ab09ebcccd687e41ac497ce065f294ff
amai-aijou <joseph.russ.olson@gmail.com>
2026-03-20 18:15:25 -0500

    13-8 exercise

---
+++ c-a-modern-approach/thirteen/exercises/13-9.c
@@ -0,0 +1,82 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 13-9.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Fri Mar 20 05:46:20 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <string.h>
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+void zero_array(char str[], int n);
+void print_array(char str[], int n);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	char s1[30];
+	char s2[30];
+	int n = 30;
+
+	zero_array(s1, n);
+	zero_array(s2, n);
+
+	strcpy(s1, "computer");
+	strcpy(s2, "science");
+
+	print_array(s1, n);
+	print_array(s2, n);
+
+	printf("c: %d | s: %d\n", 'c', 's');
+	printf("strcmp(s1, s2): %d\n", strcmp(s1, s2));
+
+	if (strcmp(s1, s2) < 0) {
+		strcat(s1, s2);
+	} else {
+		strcat(s2, s1);
+	}
+	s1[strlen(s1)-6] = '\0';
+
+	printf("Value of s1 after statements:\n");
+	print_array(s1, n);
+	printf("Value of s2 after statements:\n");
+	print_array(s2, n);
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int example_function(int i) {
+
+}
+
+void zero_array(char str[], int n) {
+
+	int i;
+
+	for (i = 0; i < n; i++) {
+		str[i] = 0;
+	}
+}
+
+void print_array(char str[], int n) {
+
+	int i;
+
+	for (i = 0; i < n; i++) {
+		printf("%c",str[i]);
+	}
+	printf("\n");
+}
