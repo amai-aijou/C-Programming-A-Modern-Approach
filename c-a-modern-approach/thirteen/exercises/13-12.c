ed659b6f77f7079d3f2b00c3c562544b839636d6
amai-aijou <joseph.russ.olson@gmail.com>
2026-03-20 19:46:57 -0500

    13-12 exercise

---
+++ c-a-modern-approach/thirteen/exercises/13-12.c
@@ -0,0 +1,80 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 13-12.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Fri Mar 20 07:09:09 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <string.h>
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+void get_extension(const char *file_name, char *extension);
+void zero_array(char str[], int n);
+void print_array(const char str[*], int n);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	const char file_name[30] = "script.sh";
+	int n = 30;
+	char extension[30];
+
+	get_extension(file_name, extension);
+
+	print_array(extension, n);
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+// appends the extension of the filename into the string
+void get_extension(const char *file_name, char *extension) {
+
+	int i;
+
+	if (strlen(file_name) == 0) {
+		printf("String has zero length.\n");
+		extension[0] = '\0';
+	}
+
+	for (i = 0; ; i++) {
+		if (file_name[i] == '.') {
+			break;
+		}
+	}
+
+	strcpy(extension, &file_name[i+1]);
+
+}
+
+void zero_array(char str[], int n) {
+
+	int i;
+
+	for (i = 0; i < n; i++) {
+		str[i] = 0;
+	}
+}
+
+void print_array(const char str[], int n) {
+
+	int i;
+
+	for (i = 0; i < n; i++) {
+		printf("%c",str[i]);
+	}
+	printf("\n");
+}
