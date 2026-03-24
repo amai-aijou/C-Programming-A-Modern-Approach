0ddb21495bd93b5143cfb639f1122a8ea47c659c
amai-aijou <joseph.russ.olson@gmail.com>
2026-03-23 22:26:23 -0500

    exercise 13-16

---
+++ c-a-modern-approach/thirteen/exercises/13-16.c
@@ -0,0 +1,65 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 13-16.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Mon Mar 23 10:19:12 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int count_spaces(const char *s);
+int count_spaces_condensed(const char *s);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	const char s[] = "this should produce four spaces.";
+
+	printf("count_spaces(): %d\n", count_spaces(s));
+	
+	printf("count_spaces_condensed(): %d\n", count_spaces_condensed(s));
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int count_spaces_condensed(const char *s) {
+
+	int count = 0;
+
+	while (*s++)  {
+		if (*s == ' ') {
+			count++;
+		}
+	}
+
+	return count;
+}
+
+int count_spaces(const char *s) {
+
+	int count = 0;
+
+	for (; *s != '\0'; s++) {
+		if (*s == ' ') {
+			count++;
+		}
+	}
+	return count;
+}
