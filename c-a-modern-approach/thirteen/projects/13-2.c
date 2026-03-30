167d05a18f90c91844873c6dfc17bf19fb425c37
amai-aijou <joseph.russ.olson@gmail.com>
2026-03-29 19:01:46 -0500

    13-2 WIP

---
+++ c-a-modern-approach/thirteen/projects/13-2.c
@@ -0,0 +1,89 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 13-2.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Thu Mar 12 09:33:11 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_REMIND 50
+#define MSG_LEN 60
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int read_line(char str[], int n);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	char reminders[MAX_REMIND][MSG_LEN+3];
+	char day_str[3], time_str[6], msg_str[MSG_LEN+1];
+	int day, hour, minute, i, j, num_remind = 0;
+
+	for (;;) {
+		if (num_remind == MAX_REMIND) {
+			printf("-- No space left --\n");
+			break;
+		}
+
+		printf("Enter date, time, and reminder: ");
+		scanf("%2d %2d:%2d", &day, &hour, &minute);
+
+		if (day == 0) {
+			break;
+
+		} else if ((day < 0) || (day > 31)) {
+
+			printf("Error: Day must be between 0 and 31.\n");
+			while (getchar() != '\n') {
+				;
+			}
+			continue;
+		}
+
+		sprintf(day_str, "%2d", day);
+		sprintf(time_str, "%2d:%2d", hour, minute);
+		read_line(msg_str, MSG_LEN);
+
+		for (i = 0; i < num_remind; i++)
+			if (strcmp(day_str, reminders[i]) < 0)
+				break;
+		for (j = num_remind; j > i; j--)
+			strcpy(reminders[j], reminders[j-1]);
+
+		strcpy(reminders[i], day_str);
+		strcat(reminders[i], time_str);
+		strcat(reminders[i], msg_str);
+
+		num_remind++;
+	}
+
+	printf("\nDay Reminder\n");
+	for (i = 0; i < num_remind; i++)
+		printf(" %s\n", reminders[i]);
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int read_line(char str[], int n) {
+	int ch, i = 0;
+
+	while ((ch = getchar()) != '\n')
+		if (i < n)
+			str[i++] = ch;
+	str[i] = '\0';
+	return i;
+}
