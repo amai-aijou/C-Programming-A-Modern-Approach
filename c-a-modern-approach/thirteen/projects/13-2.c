38b35c50832d4e05cf06f547b4c935f8a6c50174
amai-aijou <joseph.russ.olson@gmail.com>
2026-03-30 17:04:15 -0500

    13-2 project

--- c-a-modern-approach/thirteen/projects/13-2.c
+++ c-a-modern-approach/thirteen/projects/13-2.c
@@ -27,8 +27,8 @@ int read_line(char str[], int n);
 int main(void) {
 
 	char reminders[MAX_REMIND][MSG_LEN+3];
-	char day_time_str[9], msg_str[MSG_LEN+1], day_time[10];
-	int day, hour, minute, i, j, num_remind = 0;
+	char day_time_str[12], msg_str[MSG_LEN+1], day_time[10];
+	int month, day, hour, minute, i, j, num_remind = 0;
 
 	for (;;) {
 		if (num_remind == MAX_REMIND) {
@@ -36,9 +36,9 @@ int main(void) {
 			break;
 		}
 
+		printf("(Enter 0/0 0:00 to quit)\n");
 		printf("Enter date, time, and reminder: ");
-		scanf("%2d %2d:%02d", &day, &hour, &minute);
-		printf("day: %d | hour: %d | minute: %d\n", day, hour, minute);
+		scanf("%02d/%2d %2d:%02d", &month, &day, &hour, &minute);
 
 		if (day == 0) {
 			break;
@@ -52,40 +52,25 @@ int main(void) {
 			continue;
 		}
 
-		printf("-----1: reminders[%d]: |%s|\n", i, reminders[i]);
-		sprintf(day_time_str, "%2d %2d:%02d", day, hour, minute);
-		printf("sprint day_time_str: %s\n", day_time_str);
+		sprintf(day_time_str, "%02d/%2d %2d:%02d", month, day, hour, minute);
 		//sprintf(time_str, "%2d:%02d", hour, minute);
-		printf("read_line chars: %d\n", read_line(msg_str, MSG_LEN));
-		printf("after read_line - msg_str: |%s|\n", msg_str);
-		printf("-----2: reminders[%d]: |%s|\n", i, reminders[i]);
+		read_line(msg_str, MSG_LEN);
 
 		// Run through reminders[] til you hit the first day that is larger than day_str.
 		// Sorts by date, then stores the proper location in variable i
 		for (i = 0; i < num_remind; i++) {
-			printf("DEBUG - daystr: |%s| reminders[%d]: |%s|\n", day_time_str, i, reminders[i]);
 			if (strcmp(day_time_str, reminders[i]) < 0) {
-				printf("day_str is smaller than reminders[%d]\n", i);
 				break;
 			}
 		}
 
-		printf("-----3: reminders[%d]: |%s|\n", i, reminders[i]);
-		// Now sort by time
-
 		// move each reminder after the day in question down one entry to make a hole for the new entry
 		for (j = num_remind; j > i; j--) {
-			printf("\nBEFORE MOVING NUMBERS:\nreminders[%d]: |%s| reminders[%d-1]: |%s|\n", j, reminders[j], j, reminders[j-1]);
 			strcpy(reminders[j], reminders[j-1]);
-			printf("AFTER MOVING NUMBERS:\nreminders[%d]: |%s| reminders[%d-1]: |%s|\n\n", j, reminders[j], j, reminders[j-1]);
 		}
 
-		printf("reminders[%d]: |%s|\n", i, reminders[i]);
 		strcpy(reminders[i], day_time_str);
-		printf("reminders[%d] after day_time_str copy: |%s|\n", i, reminders[i]);
-		//strcat(reminders[i], time_str);
 		strcat(reminders[i], msg_str);
-		printf("reminders[%d] after masg_str copy: |%s|\n\n", i, reminders[i]);
 
 		num_remind++;
 	}
