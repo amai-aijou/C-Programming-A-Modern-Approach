a7d4f55cab9a94ab9dab1a9aa7fa6555270db1c5
amai-aijou <joseph.russ.olson@gmail.com>
2026-04-03 22:55:37 -0500

    13-6 project

---
+++ c-a-modern-approach/thirteen/projects/13-6.c
@@ -0,0 +1,64 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 13-6.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sun Mar 15 05:27:45 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define NUM_PLANETS 9
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int icstrcmp(char *s, char *t);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(int argc, char *argv[]) {
+
+	char *planets[] = {"Mercury", "Venus", "Earth",
+					   "Mars", "Jupiter", "Saturn",
+					   "Uranus", "Neptune", "Pluto"};
+	int i, j;
+
+	for (i = 1; i < argc; i++) {
+		for (j = 0; j < NUM_PLANETS; j++) {
+			if (icstrcmp(argv[i], planets[j]) == 0) {
+				printf("%s is planet %d\n", argv[i], j + 1);
+				break;
+			}
+		}
+		if (j == NUM_PLANETS) {
+			printf("%s is not a planet\n", argv[i]);
+		}
+	}
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int icstrcmp(char *s, char *t) {
+
+	int i;
+
+	for (i = 0; toupper(s[i]) == toupper(t[i]); i++) {
+
+		if (s[i] == '\0') {
+			return 0;
+		}
+	}
+	
+	return s[i] - t[i];
+}
