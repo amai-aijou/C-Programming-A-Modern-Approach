dd440dff51cf039ea89eb0bd80f27bd85a636585
amai-aijou <joseph.russ.olson@gmail.com>
2026-04-10 21:03:41 -0500

    13-14 WIP

---
+++ c-a-modern-approach/thirteen/projects/13-14.c
@@ -0,0 +1,77 @@
+/******************************************
+ * Name: 13-14.c (originally 8-16.c)
+ * Purpose: Anagram tester
+ * Author: amai-aijou
+ * Date: Wed Jan  7 09:03:09 PM CST 2026
+ ******************************************/
+
+#include <stdio.h>
+#include <ctype.h> // necessary for tolower() and toalpha()
+
+// ALPHA is size of array dimension; SIZE is size of all array dimensions
+#define ALPHA 26
+
+// Prototypes
+bool are_anagrams(const char *word1, const char *word2);
+
+int main(void) {
+
+	int array[ALPHA] = {0};
+	char ch;
+	int i;
+	int sum = 0;
+
+	// Zero out array before starting
+	for (i=0; i < ALPHA; i++) {
+		array[i] = 0;
+	}
+
+	// Ask for first word
+	printf("Enter first word: ");
+	
+	// Loop through input; for each char, increment array position for each alpha letter by 1
+	while ((ch = getchar()) != '\n') {
+		//Confirm character is a letter
+		if (isalpha(ch)) {
+			
+			// Increment array; tolower() ensures ASCII codes match, because 97 ('a') is then subtracted to ensure array is 0-25
+			// ex: ch = 'b' = "98-97" = "0"; thus, array[0] would be updated 
+			array[((tolower(ch))-97)]++;
+		}
+	}
+
+	// Ask for second word
+	printf("Enter second word: ");
+
+	// Loop through input; for each char, decrement array position for each alpha letter by 1
+	while ((ch = getchar()) != '\n') {
+		//Confirm character is a letter
+		if (isalpha(ch)) {
+			
+			// Decrement array; tolower() ensures ASCII codes match, because 97 ('a') is then subtracted to ensure array is 0-25
+			// ex: ch = 'b' = "98-97" = "0"; thus, array[0] would be updated 
+			array[((tolower(ch))-97)]--;
+		}
+	}
+
+	// If the two are anagrams, the sum total of all array elements will be 0.
+	for (i=0; i < ALPHA; i++) {
+		sum += array[i];
+	}
+
+	if (sum == 0) {
+		printf("The words are anagrams.\n");
+	}
+
+	if (sum > 0 ) {
+		printf("The words are not anagrams.\n");
+	}
+
+	return 0;
+}
+
+bool are_anagrams(const char *word1, const char *word2);
+
+/*****DEBUGGING*****
+printf("test: %d\n", array[(ch-97)]);
+*/
