4cf8946d1e1438f3b608cfc946a209c12ab8baf6
amai-aijou <joseph.russ.olson@gmail.com>
2026-04-16 14:39:30 -0500

    13-17 done

---
+++ c-a-modern-approach/thirteen/projects/13-17.c
@@ -0,0 +1,89 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 13-17.c (formerly 12-2.c)
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Thu Mar  5 08:24:49 PM CST 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+// UPDATE: 13-17, like most of my Chapter 12 assignments, already uses functions.
+// Thus, the only difference is whether the array is populated via function, or by a string literal
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define MAX 100
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+void read_user_input_arith(char a[*], int *n);
+bool check_palindrome(char a[*], int *n);
+void print_array(char a[*], int *n);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	int n = 7;
+	char a[MAX] = "racecar";
+
+	read_user_input_arith(a, &n);
+
+	if (check_palindrome(a, &n)) {
+		printf("Palindrome.\n");
+	} else {
+		printf("Not a palindrome.\n");
+	}
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+void read_user_input_arith(char a[], int *n) {
+
+	char *p = a;
+	char ch;
+
+	printf("Enter a message: ");
+	while ((ch = getchar()) != '\n') {
+		if (isalpha(ch)) {
+			*(p + (*n)++) = tolower(ch);	
+		}
+	}
+}
+
+bool check_palindrome(char a[], int *n) {
+
+	char *p = a;
+	char ch;
+	int x = 0;
+
+	for (p = a; p <= a + (*n/2); p++) {
+
+		if (*p != *(p + (*n-1) - x)) {	
+			return false;
+		}
+		x += 2;
+	}
+
+	return true;
+}
+
+void print_array(char a[], int *n) {
+
+	char *p;
+
+	for (p = a; p < a + *n; p++) {
+		printf("%c ", *p);
+	}
+	printf("\n");
+}
