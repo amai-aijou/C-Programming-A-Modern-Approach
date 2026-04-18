74a6b596c2fafc6935a40437e30d5014711d7a0f
amai-aijou <joseph.russ.olson@gmail.com>
2026-04-17 21:50:13 -0500

    13-18 - Chapter 13 FINISHED!

--- c-a-modern-approach/thirteen/projects/13-18.c
+++ c-a-modern-approach/thirteen/projects/13-18.c
@@ -19,18 +19,39 @@
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                   ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-void print_date(int month, int day, int year);
+void print_date(int *month, int *day, int *year);
+void read_user_input(int *month, int *day, int *year);
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                 ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 int main(void) {
 
+	int month, day, year;
+
+	read_user_input(&month, &day, &year);
+
+	print_date(&month, &day, &year);
+
+	return 0;
 }
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                    ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-void print_date(int month, int day, int year) {
+void read_user_input(int *month, int *day, int *year) {
 
+	char ch;
+	
+	printf("Enter a date (mm/dd/yyyy): ");
+	scanf("%2d/%2d/%4d", month, day, year);	
+
+}
+
+void print_date(int *month, int *day, int *year) {
+	
+	char *month_names[] = {"January", "February", "March", "April", "May", "June", "July",
+							"August", "September", "October", "November", "December"};
+
+	printf("You entered the date %s %2d, %4d\n", month_names[(*month-1)], *day, *year);
 }
