7e79a91b7a157ed754dd7c044e151fbbf40fa453
amai-aijou <joseph.russ.olson@gmail.com>
2026-04-20 19:29:06 -0500

    14-1 touch-ups

--- c-a-modern-approach/fourteen/exercises/14-1.c
+++ c-a-modern-approach/fourteen/exercises/14-1.c
@@ -25,16 +25,20 @@
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 int main(void) {
 
-	int a = 3;
+	int a = 5;
 	int b = 13;
 	int c1 = 25, c2 = 5;
+	int *p1 = &c1, *p2 = &c2;
 
 	printf("(a) %d to the power of three is: %d\n", a, CUBED(a));
 	printf("(b) The remainder of %d when divided by four is: %d\n", b, MOD4(b));
 	printf("(c) The product of %d and %d is (1), or is not (0), less than 100: %d\n", c1, c2, HYAKUPROD(c1,c2));
 
 	printf("\nWould your Macros always work? If not, describe what arguments would make them fail:\n");
-	printf(" ");
+	printf("If this fails, it's only because you didn't want it bad enough!\n"
+			 "okay, fine, maybe it's because there's a typecasting issue...or you tried using a pointer, like so:\n");
+	printf("ex.: %d to the power of three is: %d\n", a, CUBED(a));
+	printf("...well, okay, so that worked...maybe my code is just amazing then =P\n");
 
 	return 0;
 }
