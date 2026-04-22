185816910e9f3808f5d3cfcac412a07586907627
amai-aijou <joseph.russ.olson@gmail.com>
2026-04-21 21:46:27 -0500

    14-6 Exercise

---
+++ c-a-modern-approach/fourteen/exercises/14-6.c
@@ -0,0 +1,62 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 14-6.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Tue Apr 21 09:19:19 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define DISP(f,x) (printf("(a) Function return value: %d\n", f(x)))
+#define DISP2(f,x,y) (printf("(b) Function return value: %d\n", f(x,y)))
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int square_root_close_enough(int x);
+int product(int x, int y);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	DISP(square_root_close_enough, 25);
+
+	DISP2(product, 5, 5);
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+// I know math.h exists, but I wanted to think about how to actually do this
+// I'll refine this with the Newton-Raphson Method when I feel like it...someday
+int square_root_close_enough(int x) {
+
+	int i = 0;
+	
+	while (i * i <= x) {
+
+		if ((i + 1) * (i + 1) <= x) {
+
+			i++;
+			// to bring i up to a decent whole number
+		} else {
+
+			break;
+		}
+	}
+
+	return i;
+}
+
+int product(int x, int y) {
+
+	return x * y;
+}
