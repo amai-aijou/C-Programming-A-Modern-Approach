78a036fd5030866017c7d4adc453fdeed69185ec
amai-aijou <joseph.russ.olson@gmail.com>
2026-04-21 21:01:35 -0500

    14-5 Exercise

---
+++ c-a-modern-approach/fourteen/exercises/14-5.c
@@ -0,0 +1,55 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 14-5.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Tue Apr 21 08:24:08 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <string.h>
+
+#define TOUPPER(c) ('a'<=(c)&&(c)<='z'?(c)-'a'+'A':(c))
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	char s[50];
+	int i = 0;
+
+	printf("\n");
+	printf("(a) strcpy(s, \"abcd\"); i = 0; putchar(TOUPPER(s[++i]));\n");
+	printf("  Output: ");
+	strcpy(s, "abcd");
+	i = 0;
+	putchar(TOUPPER(s[++i]));
+	printf("\n  Answer: The macro has four calls to (c), so ++i happens 4x!\n\n");
+
+
+	printf("(b) strcpy(s, \"0123\"); i = 0; putchar(TOUPPER(s[++i]));\n");
+	printf("  Output: ");
+	strcpy(s, "0123");
+	i = 0;
+	putchar(TOUPPER(s[++i]));
+	printf("\n  Answer: It only uses the 'else' portion of the 'if:then?else' statement, so it happens twice.\n\n");
+
+	printf("The big takeaway is that ++i/i++ seems to trigger once for each 'pass' of the pre-processor...\n");
+	printf("  ...and once more for the actual codebase, so it will generally trigger twice at *minimum*!\n");
+	printf("\n");
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
