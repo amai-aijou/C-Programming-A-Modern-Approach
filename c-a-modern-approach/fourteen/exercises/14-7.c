429cd867400856794f69b6a8b751000b60c814c2
amai-aijou <joseph.russ.olson@gmail.com>
2026-04-23 22:45:30 -0500

    14-7 exercise

---
+++ c-a-modern-approach/fourteen/exercises/14-7.c
@@ -0,0 +1,52 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 14-7.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Wed Apr 22 04:30:09 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+typedef unsigned long ulong;
+
+#define GENERIC_MAX(type)		\
+type type##_max(type x, type y)	\
+{								\
+	return x > y ? x : y;		\
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	GENERIC_MAX(long);
+
+	printf("(a) The preprocessor expansion (as evidenced by gcc -E) is:\n");
+	printf("long long_max(long x, long y) { return x > y ? x : y; };\n");
+
+	GENERIC_MAX(unsigned long);
+
+	printf("(b) Preprocessor expansion tries to name the function 'unsigned long_max', which is illegal:\n");
+ 	printf("unsigned long unsigned long_max(unsigned long x, unsigned long y) { return x > y ? x : y; };\n");
+
+	GENERIC_MAX(ulong);
+
+	printf("(c) Adding 'typedef unsigned long ulong' to the top of the program results in 'ulong_max', a perfectly legal name:\n");
+ 	printf("ulong ulong_max(ulong x, ulong y) { return x > y ? x : y; };\n");
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
