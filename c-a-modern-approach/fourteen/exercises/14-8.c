2ee28ee04bc72cf4f8b0b07c54843d6a228ee08b
amai-aijou <joseph.russ.olson@gmail.com>
2026-04-25 14:14:44 -0500

    14-8 final cleanup

--- c-a-modern-approach/fourteen/exercises/14-8.c
+++ c-a-modern-approach/fourteen/exercises/14-8.c
@@ -12,8 +12,15 @@
 
 #include <stdio.h>
 
+// 5. Evaluates to: "Line 32 of file" "14-8.c"
+// 4. Turns STR(Line 32 of file) into: "Line 32 of file" __FILE__
 #define STR(x) #x " " __FILE__
+
+// 3. This intermediary forces __LINE__ to evaluate before being stringized!
+// 2. Calls STR(Line 32 of file)
 #define STR2(x) STR(x)
+
+// 1. Calls STR2(Line __LINE__ of file)
 #define LINE_FILE STR2(Line __LINE__ of file)
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -21,10 +28,10 @@
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 int main(void) {
 
-	// const char *str = "Line 25 of file" " " "14-8.c";
+	//Evaluates to: const char *str = "Line 32 of file" " " "14-8.c";
 	const char *str = LINE_FILE;
 
-	// Prints: Line 25 of file 14-8.c
+	// Prints: Line 32 of file 14-8.c
 	printf("%s\n", str);
 
 	return 0;
