a84f91a81763801cb6f158a2e12e8b95a31442cc
amai-aijou <joseph.russ.olson@gmail.com>
2026-04-26 15:54:51 -0500

    exercise 14-12

---
+++ c-a-modern-approach/fourteen/exercises/14-12.c
@@ -0,0 +1,45 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 14-12.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sun Apr 26 03:41:39 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define M 10
+
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+#if M		// TRUE
+printf("(a) Value of M: %d\n", M);
+#endif
+
+#ifdef M	// TRUE
+printf("(b) Value of M: %d\n", M);
+#endif
+
+#ifndef M	// FALSE
+printf("(c) Value of M: %d\n", M);
+#endif
+
+#if defined M // TRUE
+printf("(d) Value of M: %d\n", M);
+#endif
+
+#if !defined M // FALSE
+printf("(e) Value of M: %d\n", M);
+#endif
+
+	return 0;
+}
+
