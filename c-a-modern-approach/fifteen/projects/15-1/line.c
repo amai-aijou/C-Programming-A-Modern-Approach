f243b3596c223979d714299470fd8ac0a5754fce
amai-aijou <joseph.russ.olson@gmail.com>
2026-05-05 19:22:12 -0500

    Project 15-2 complete

--- c-a-modern-approach/fifteen/projects/15-1/line.c
+++ c-a-modern-approach/fifteen/projects/15-1/line.c
@@ -35,20 +35,22 @@ int space_remaining(void) {
 void write_line(void) {
 	int extra_spaces, spaces_to_insert, i, j;
 
-	printf("DEBUG: line_len: %d | MAX_LINE_LEN: %d\n");
-
 	extra_spaces = MAX_LINE_LEN - line_len;
+//	printf("----------------------------------------------\n");
+//	printf("num_lines: %d\n", num_lines);
+//	printf("extra_spaces: %d | DEBUG: line_len: %d | MAX_LINE_LEN: %d\n", extra_spaces, line_len, MAX_LINE_LEN );
+
 	for (i = 0; i < line_len; i++) {
-		if (line[i] != ' ')
+		if (line[i] != ' ') {
 			putchar(line[i]);
-		else {
+		} else {
+			// 15-1 Project: Alternate justification between beginning and end of line
 			if (num_lines % 2 == 0) {
-				spaces_to_insert = extra_spaces / (num_words - 1);
-				printf("DEBUG: spaces to insert: %d | extra_spaces: %d | num_words: %d\n", spaces_to_insert, extra_spaces, num_words);
+				spaces_to_insert = extra_spaces  / (num_words - 1);
+//				printf("\nDEBUG: spaces to insert = (extra_spaces / (num_words - 1): %d | extra_spaces: %d | num_words: %d\n", spaces_to_insert, extra_spaces, num_words);
 			} else {
-				// 15-1 Project: Alternate justification between beginning and end of line
-				spaces_to_insert = extra_spaces / (num_words - 2);
-				printf("DEBUG: spaces to insert: %d | extra_spaces: %d | num_words: %d\n", spaces_to_insert, extra_spaces, num_words);
+				spaces_to_insert = (extra_spaces) / (num_words - 1);
+//				printf("\nDEBUG: spaces to insert (extra_spaces / (num_words - 1): %d | extra_spaces: %d | num_words: %d\n", spaces_to_insert, extra_spaces, num_words);
 			}
 			for (j = 1; j <= spaces_to_insert + 1; j++)
 				putchar(' ');
@@ -67,13 +69,22 @@ void flush_line(void) {
 	}
 }
 
+
+
+// 9 / 6 ( 9 - 1)
+
 // insert behavior when line is even:
 // spaces_to_insert = 10 / (5 - 1) = 2
 // 8 / (4 - 1) = 2
 // 6 / (3 - 1) = 3
 // 3 / (2 - 1) = 3
 // 0 / 0
-
+/*
+10 + 1 / 5 - 1 = 1
+9 + 1 / 4 - 1 = 2
+7 + 1 / 3 - 1 = 2
+5 + 1 / 2 - 1 = 6
+*/
 // spaces_to_insert = 10 / 5 - 2 = 3
 // 7 / 4 - 2 = 3
 // 4 / 3 - 1 = 2
