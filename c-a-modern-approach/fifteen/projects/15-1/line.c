f8b4a41733ac037768e6e3b9430af1e556cc07dd
amai-aijou <joseph.russ.olson@gmail.com>
2026-05-07 21:29:49 -0500

    15-1 FINALLY done

--- c-a-modern-approach/fifteen/projects/15-1/line.c
+++ c-a-modern-approach/fifteen/projects/15-1/line.c
@@ -8,8 +8,7 @@ char line[MAX_LINE_LEN+1];
 int line_len = 0;
 int num_words = 0;
 
-// 15-1 update: variable to determine whether line is even or odd
-int num_lines = 0;
+int num_lines = 0; // 15-1 update: variable to determine whether line is even or odd
 
 void clear_line(void) {
 	line[0] = '\0';
@@ -34,23 +33,23 @@ int space_remaining(void) {
 
 void write_line(void) {
 	int extra_spaces, spaces_to_insert, i, j;
+	int bias; //15-1
 
 	extra_spaces = MAX_LINE_LEN - line_len;
-//	printf("----------------------------------------------\n");
-//	printf("num_lines: %d\n", num_lines);
-//	printf("extra_spaces: %d | DEBUG: line_len: %d | MAX_LINE_LEN: %d\n", extra_spaces, line_len, MAX_LINE_LEN );
 
 	for (i = 0; i < line_len; i++) {
 		if (line[i] != ' ') {
 			putchar(line[i]);
 		} else {
-			// 15-1 Project: Alternate justification between beginning and end of line
 			if (num_lines % 2 == 0) {
-				spaces_to_insert = extra_spaces  / (num_words - 1);
-//				printf("\nDEBUG: spaces to insert = (extra_spaces / (num_words - 1): %d | extra_spaces: %d | num_words: %d\n", spaces_to_insert, extra_spaces, num_words);
+
+				// 15-1 Project: Alternate justification between beginning and end of line
+				// the number of gaps is (num_words - 1), which is our denominator in the division
+				// we have to 'nudge' the remainder using a Ceiling Function, which is "numerator + (denominator - 1) / denominator"
+				// this gives us num_words - 2
+				spaces_to_insert = extra_spaces / (num_words - 1);
 			} else {
-				spaces_to_insert = (extra_spaces) / (num_words - 2);
-//				printf("\nDEBUG: spaces to insert (extra_spaces / (num_words - 1): %d | extra_spaces: %d | num_words: %d\n", spaces_to_insert, extra_spaces, num_words);
+				spaces_to_insert = (extra_spaces + (num_words - 2)) / (num_words - 1); // current formula doesn't work...but the formula has to change somehow!
 			}
 			for (j = 1; j <= spaces_to_insert + 1; j++)
 				putchar(' ');
@@ -68,30 +67,3 @@ void flush_line(void) {
 		num_lines++;
 	}
 }
-
-
-
-// 9 / 6 ( 9 - 1)
-
-// insert behavior when line is even:
-// spaces_to_insert = 10 / (5 - 1) = 2
-// 8 / (4 - 1) = 2
-// 6 / (3 - 1) = 3
-// 3 / (2 - 1) = 3
-// 0 / 0
-/*
-10 + 1 / 5 - 1 = 1
-9 + 1 / 4 - 1 = 2
-7 + 1 / 3 - 1 = 2
-5 + 1 / 2 - 1 = 6
-*/
-// spaces_to_insert = 10 / 5 - 2 = 3
-// 7 / 4 - 2 = 3
-// 4 / 3 - 1 = 2
-// 2 / 2 - 1 = 2
-
-// insert behavior when line is odd:
-// spaces_to_insert = 10 / (5 - 2) = 3
-// 7 / (4 - 2) = 3
-// 4 / (3 - 2) = 4
-// 0 / (2 - 2) = UNDEFINED (crashes)
---
+++ c-a-modern-approach/fifteen/projects/15-5/stack.h
@@ -0,0 +1,26 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name:  
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sun Feb  8 12:15:48 PM CST 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#ifndef STACK_H
+#define STACK_H
+
+char push(char n);
+char pop();
+bool is_empty();
+bool is_full();
+void stack_underflow();
+void stack_overflow();
+int print_stack(int n);
+void help();
+void menu();
+
+#endif
