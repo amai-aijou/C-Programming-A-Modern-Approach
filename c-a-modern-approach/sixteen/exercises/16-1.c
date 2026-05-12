b636b0336288461081f0585e7b316b999349e951
amai-aijou <joseph.russ.olson@gmail.com>
2026-05-11 20:48:35 -0500

    16-1 Exercise

---
+++ c-a-modern-approach/sixteen/exercises/16-1.c
@@ -0,0 +1,45 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 16-1.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Mon May 11 08:40:01 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	struct { int x, y;} x;
+	struct { int x, y;} y;
+
+	printf("Q. Are these declarations legal on an individual basis?\n"
+			"Could both Declarations appear as shown in a program?\n\n");
+
+	printf("A. These are both legal, but will not have any meaningful values yet.\n\n");
+
+	printf("Before values assigned to x or y:\n");
+	printf("x.x: %d\n\n", x.x);
+
+	x.x = 4;
+	x.y = 5;
+	y.x = 6;
+	y.y = 7;
+
+	printf("After values assigned to x and y in each struct:\n");
+	printf("x.x: %d\n", x.x);
+	printf("x.y: %d\n", x.y);
+	printf("y.x: %d\n", y.x);
+	printf("y.y: %d\n", y.y);
+
+	return 0;
+}
+
