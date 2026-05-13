1a2f69e99c57903c10c74196d32f20e0296ca367
amai-aijou <joseph.russ.olson@gmail.com>
2026-05-12 19:16:12 -0500

    16-4 Exercises

--- c-a-modern-approach/sixteen/exercises/16-4.c
+++ c-a-modern-approach/sixteen/exercises/16-4.c
@@ -1,9 +1,9 @@
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                  ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
-   ❤︎︎࣪ Name: test.c
+   ❤︎︎࣪ Name: 16-4.c
    ❤︎︎࣪ Purpose: 
    ❤︎︎࣪ Author: amai-aijou
-   ❤︎︎࣪ Date: Tue May 12 06:23:32 PM CDT 2026
+   ❤︎︎࣪ Date: Tue May 12 06:12:23 PM CDT 2026
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
                                                                 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -11,27 +11,73 @@
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 
 #include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <time.h>
+
+// (a)
+typedef struct {
+	double real;
+	double imaginary;
+} complex;
+
+
+
+struct complex {
+	double real;
+	double imaginary;
+};
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                   ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-int example_function(int i);
+struct complex make_complex(double real, double imaginary);
+struct complex add_complex(struct complex c1, struct complex c2);
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                 ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 int main(void) {
 
+
+	// (b)
+	struct complex c1, c2, c3;
+
+	// (c)
+	c1 = make_complex(2.0, 3.0);
+	c2 = make_complex(4.0, 5.0);
+
+	// (d)
+	c3 = add_complex(c1, c2);
+
+	printf("c1.real: %.1f\n", c1.real);
+	printf("c1.imaginary: %.1f\n", c1.imaginary);
+	printf("c2.real: %.1f\n", c2.real);
+	printf("c2.imaginary: %.1f\n\n", c2.imaginary);
+
+	printf("After c1 + c2 addition::\n");
+	printf("c3.real: %.1f\n", c3.real);
+	printf("c3.imaginary: %.1f\n\n", c3.imaginary);
+
+	return 0;
 }
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                    ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-int example_function(int i) {
+struct complex make_complex(double real, double imaginary) {
 
+	struct complex c;
+
+	c.real = real;
+	c.imaginary = imaginary;
+
+	return c;
 }
 
+struct complex add_complex(struct complex c1, struct complex c2) {
+
+	struct complex c;
+
+	c.real = c1.real + c2.real;
+	c.imaginary = c1.imaginary + c2.imaginary;
+
+	return c;
+}
