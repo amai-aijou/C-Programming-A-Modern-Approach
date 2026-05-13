a179b078b4aa0fba64c45beca258057a2c29cb9e
amai-aijou <joseph.russ.olson@gmail.com>
2026-05-12 19:11:59 -0500

    16-3 Exercise

---
+++ c-a-modern-approach/sixteen/exercises/16-3.c
@@ -0,0 +1,76 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 16-3.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Tue May 12 06:12:23 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+// (a)
+struct complex {
+	double real;
+	double imaginary;
+};
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+struct complex make_complex(double real, double imaginary);
+struct complex add_complex(struct complex c1, struct complex c2);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+
+	// (b)
+	struct complex c1, c2, c3;
+
+	// (c)
+	c1 = make_complex(2.0, 3.0);
+	c2 = make_complex(4.0, 5.0);
+
+	// (d)
+	c3 = add_complex(c1, c2);
+
+	printf("c1.real: %.1f\n", c1.real);
+	printf("c1.imaginary: %.1f\n", c1.imaginary);
+	printf("c2.real: %.1f\n", c2.real);
+	printf("c2.imaginary: %.1f\n\n", c2.imaginary);
+
+	printf("After c1 + c2 addition::\n");
+	printf("c3.real: %.1f\n", c3.real);
+	printf("c3.imaginary: %.1f\n\n", c3.imaginary);
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+struct complex make_complex(double real, double imaginary) {
+
+	struct complex c;
+
+	c.real = real;
+	c.imaginary = imaginary;
+
+	return c;
+}
+
+struct complex add_complex(struct complex c1, struct complex c2) {
+
+	struct complex c;
+
+	c.real = c1.real + c2.real;
+	c.imaginary = c1.imaginary + c2.imaginary;
+
+	return c;
+}
