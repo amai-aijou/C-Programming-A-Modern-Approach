2fc175670984ea441dd8b6fb647b4b7cdf287db9
amai-aijou <joseph.russ.olson@gmail.com>
2026-05-31 23:48:25 -0500

    17-11 Exercise: designed a recursive linked_list creator!

---
+++ c-a-modern-approach/seventeen/exercises/17-11.c
@@ -0,0 +1,88 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 17-11.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sun May 31 06:46:20 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int value;
+	struct node *next;
+};
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int count_occurrences(struct node *list, int n);
+struct node *create_linked_list_recursive(struct node *list, int numNodes);
+void *print_list(struct node *list);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	struct node *first = NULL;
+
+	first = create_linked_list_recursive(first, 5);
+
+	printf("Printing again after function: \n");
+	print_list(first);
+
+//	count_occurrences(first, n);
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int count_occurrences(struct node *list, int n) {
+
+}
+
+// I made it recursive because I'm ridiculous like that
+struct node *create_linked_list_recursive(struct node *list, int numNodes) {
+
+	struct node *new_node = malloc(sizeof(struct node));
+	
+	if (new_node == NULL) {
+		printf("ERROR: Memory could not be allocated. Exiting to prevent null pointer!\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	new_node->value = (numNodes * 10);
+
+	if (numNodes > 1 ) {
+		new_node->next = create_linked_list_recursive(new_node, (numNodes - 1));
+	} else {
+		print_list(list);
+	}
+
+	return new_node;
+}
+
+void *print_list(struct node *list) {
+
+	struct node *p;
+
+	printf("[");
+	for (p = list; p != NULL; p = p->next) {
+
+		if (p != list) {
+			printf(" -> ");
+		}
+
+		printf("%d", p->value);
+	}
+	printf("]");
+	printf("\n\n");
+}
