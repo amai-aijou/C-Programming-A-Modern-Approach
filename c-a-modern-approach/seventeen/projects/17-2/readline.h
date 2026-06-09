a5eff9180f5f7c8ff7b9ce448927e80ce72305ff
amai-aijou <joseph.russ.olson@gmail.com>
2026-06-09 17:48:51 -0500

    17-2 Project

--- c-a-modern-approach/seventeen/projects/17-2/readline.h
+++ c-a-modern-approach/seventeen/projects/17-2/readline.h
@@ -1,58 +1,31 @@
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                  ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
-   ❤︎︎࣪ Name: 17-18.c
+   ❤︎︎࣪ Name: readline.h
    ❤︎︎࣪ Purpose: 
    ❤︎︎࣪ Author: amai-aijou
-   ❤︎︎࣪ Date: Sun Jun  7 03:45:23 PM CDT 2026
+   ❤︎︎࣪ Date: Sun May 10 08:28:26 PM CDT 2026
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
                                                                 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                       ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 
-#include <stdio.h>
+#ifndef READLINE_H
+#define READLINE_H
 
-#define NAME_LEN 25
-
-struct part {
-	int number;
-	char name[NAME_LEN+1];
-		int on_hand;
-		struct part *next;
-	};
-
-struct part *inventory = NULL;
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                   ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-int compare_parts(const void *p, const void *q);
-int compare_parts_descending(const void *p, const void *q);
+
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+    read_line: Skips leading white-space characters, then
+			   reads the remainder of the input line and
+			   stores it in str. Truncates the line if its
+			   length exceeds n. Returns the number of
+			   characters stored.
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-int main(void) {
+int read_line(char str[], int n);
 
-	return 0;
-}
-
-/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
-  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-int compare_parts(const void *p, const void *q) {
-
-	const struct part *p1 = p;
-	const struct part *q1 = q;
-
-	if (p1->number < q1->number) {
-		return -1;
-	} else if (p1->number == q1->number) {
-		return 0;
-	} else {
-		return 1;
-	}
-}
-
-int compare_parts_descending(const void *p, const void *q) {
-}
+#endif
