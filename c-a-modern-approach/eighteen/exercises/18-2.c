4a08aa75b287ed6835ffbcf5387e15f7a1c8300e
amai-aijou <joseph.russ.olson@gmail.com>
2026-06-20 20:02:14 -0500

    18-2 Exercise

--- c-a-modern-approach/eighteen/exercises/18-2.c
+++ c-a-modern-approach/eighteen/exercises/18-2.c
@@ -1,6 +1,6 @@
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                  ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
-   ❤︎︎࣪ Name: 18-1.c
+   ❤︎︎࣪ Name: 18-2.c
    ❤︎︎࣪ Purpose: 
    ❤︎︎࣪ Author: amai-aijou
    ❤︎︎࣪ Date: Sat Jun 20 06:14:55 PM CDT 2026
@@ -12,6 +12,13 @@
 
 #include <stdio.h>
 
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                   ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
@@ -22,40 +29,19 @@
 int main(void) {
 
 	printf("-------------------EXERCISE 18-1-------------------\n\n");
-	printf("Q. For each of the following declarations, identify the:\n"
-		   "  * Storage Class\n"
-		   "  * Type Qualifiers\n"
-		   "  * Type Specifiers\n"
-		   "  * Declarators\n"
-		   "  * Initializers\n\n");
+	printf(COLOR_MAGENTA "Answer each of the following questions with auto, extern, register, and/or static.\n\n");
 
-	printf("(a) static char **lookup(int level);\n"
-		   "  * Storage Class: static\n"
-		   "  * Type Qualifiers: -\n"
-		   "  * Type Specifiers: char\n"
-		   "  * Declarators: **lookup(int level), level\n"
-		   "  * Initializers:\n\n");
+	printf(COLOR_CYAN "(a) Which storage class is used primarily to indicate that a variable or function can\n"
+		   "    be shared by several files?\n");
+	printf(COLOR_RESET "    A. extern\n\n");
 
-	printf("(b) volatile unsigned long io_flags;\n"
-		   "  * Storage Class: -\n"
-		   "  * Type Qualifiers: volatile\n"
-		   "  * Type Specifiers: unsigned long\n"
-		   "  * Declarators: io_flags\n"
-		   "  * Initializers: -\n\n");
+	printf(COLOR_CYAN "(b)  Q. Suppose that a variable x is to be shared by several functions in one file, but\n"
+		   "        hidden from functions in other files.\n"
+		   "        Which storage class should x be declared to have?\n");
+	printf(COLOR_RESET "    A. static (but know that it is *truly* shared, and the functions can each manipulate the value)\n\n");
 
-	printf("(c) extern char *file_name[MAX_FILES], path[];\n"
-		   "  * Storage Class: extern\n"
-		   "  * Type Qualifiers: -\n"
-		   "  * Type Specifiers: char\n"
-		   "  * Declarators: *file_name[MAX_FILES], path[]\n"
-		   "  * Initializers:\n\n");
-	
-	printf("(d) static const char token_buf[] = "";\n"
-		   "  * Storage Class: static\n"
-		   "  * Type Qualifiers: const\n"
-		   "  * Type Specifiers: char\n"
-		   "  * Declarators: token_buf[]\n"
-		   "  * Initializers: ""\n\n");
+	printf(COLOR_CYAN "(c)  Q. Which storage classes can affect the storage duration of a variable?\n");
+	printf(COLOR_RESET "    A. auto, static, register\n\n");
 
 	return 0;
 }
