0f1a10dab72e9601a854fa8e597c3c375cc3276c
amai-aijou <joseph.russ.olson@gmail.com>
2026-06-21 16:03:43 -0500

    18-3 Exercise

---
+++ c-a-modern-approach/eighteen/exercises/18-3.c
@@ -0,0 +1,53 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 18-3.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sat Jun 20 06:14:55 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	printf("-------------------EXERCISE 18-3-------------------\n\n");
+	printf(COLOR_MAGENTA "List the storage duration (static or automatic), scope (block or file),\n"
+						 "and linkage (internal, external, or none) of each variable and parameter in the following file:\n\n");
+	printf(COLOR_RESET "extern float a;\n\n"
+			           "void f(register double b)\n"
+					   "{\n"
+					   "    static int c;\n"
+					   "    auto char d;\n"
+					   "}\n\n");
+
+	printf(COLOR_CYAN "extern float a: " COLOR_RESET"\tstatic |  file | external \n");
+	printf(COLOR_CYAN "void f: " COLOR_RESET"\t\tstatic |  file | external" COLOR_CYAN "\t(it's not a parameter or variable, but it looked lonely, so...)\n");
+	printf(COLOR_CYAN "register double b: " COLOR_RESET"\t  auto | block | none\n");
+	printf(COLOR_CYAN "static int c: " COLOR_RESET"\t\tstatic | block | none\n");
+	printf(COLOR_CYAN "auto char d: " COLOR_RESET"\t\t  auto | block | none\n\n");
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
