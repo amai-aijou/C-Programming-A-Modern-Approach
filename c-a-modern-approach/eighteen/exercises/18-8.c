d308ead75fd0d637a18818b470829eab327c91fd
amai-aijou <joseph.russ.olson@gmail.com>
2026-06-23 21:56:25 -0500

    18-8 Exercise

---
+++ c-a-modern-approach/eighteen/exercises/18-8.c
@@ -0,0 +1,54 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 18-8.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Tues Jun 23 06:14:55 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	printf("-------------------EXERCISE 18-8-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. Fully describe the type of x in each declaration:\n\n");
+
+	printf(COLOR_CYAN "(a) char (*x[10])(int);\n");
+	printf(COLOR_MAGENTA "x is: " COLOR_RESET "an array of ten function pointers that have an int argument and return a char\n\n");
+
+	printf(COLOR_CYAN "(b) int (*x(int))[5];\n");
+	printf(COLOR_MAGENTA "x is: " COLOR_RESET "a function with an int argument that returns an array of five int pointers\n\n");
+	printf(COLOR_MAGENTA "x is: " COLOR_RESET "a function with an int argument and returns an pointer to an array of five ints\n\n");
+
+	printf(COLOR_CYAN "(c) float *(*x(void))(int);\n");
+	printf(COLOR_MAGENTA "x is: " COLOR_RESET "a function with a void parameter that returns a pointer to a function with an int parameter and a float return type \n\n");
+
+	printf(COLOR_CYAN "(d) void (*x(int, void (*y)(int)))(int);\n");
+	printf(COLOR_MAGENTA "x is: " COLOR_RESET "a function with an int parameter and a void parameter y (a function pointer that takes an int),\n"
+		                 "which returns a void function pointer that takes an int\n\n");
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
