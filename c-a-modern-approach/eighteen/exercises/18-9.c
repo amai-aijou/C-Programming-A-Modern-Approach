f8ef3fa936996c003f393ff6170600fea9bf442e
amai-aijou <joseph.russ.olson@gmail.com>
2026-06-25 00:13:54 -0500

    18-9 contd

--- c-a-modern-approach/eighteen/exercises/18-9.c
+++ c-a-modern-approach/eighteen/exercises/18-9.c
@@ -19,26 +19,29 @@
 #define COLOR_CYAN "\033[1;36m"
 #define COLOR_RESET "\033[1;0m"
 
-// (a) char (*v[10])(int);
+// (a) char (*x[10])(int);
 typedef char Fcn_a(int);
 typedef Fcn_a *Fcn_ptr_a;
 typedef Fcn_ptr_a Fcn_ptr_array_a[10];
 typedef Fcn_ptr_array_a v;
 
-// (b) int (*w(int))[5];
+// (b) int (*x(int))[5];
 typedef int Fcn_ptr_b[5];
 typedef Fcn_ptr_b *Fcn_int_b;
 typedef Fcn_int_b Fcn_b(int);
-typedef Fcn_b w;
+typedef Fcn_b x;
 
 // (c) float *(*x(void))(int);
-typedef float *Fcn_fcn(int);
-typedef Fcn_fcn Fcn_ptr(int);
-typedef Fcn_ptr *Fcn_void_c;
+typedef float *Fcn_ptr_c(int);
+typedef Fcn_ptr_c *Fcn_void_c;
 typedef Fcn_void_c Fcn_c(void);
 typedef Fcn_c x;
 
-// (d) void (*y(int, void (*z)(int)))(int);\n");
+// (d) void (*x(int, void (*y)(int)))(int);\n");
+
+// build y first
+typedef  Fcn_y;
+typedef Fcn_y y;
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                 ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
