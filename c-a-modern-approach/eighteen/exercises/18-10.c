8bb4915b9eb2bbc70c7c936c68b1556f222c1be6
amai-aijou <joseph.russ.olson@gmail.com>
2026-06-26 17:35:21 -0500

    Exercise 18-10

---
+++ c-a-modern-approach/eighteen/exercises/18-10.c
@@ -0,0 +1,57 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 18-10.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Fri Jun 26 06:14:55 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	printf("-------------------EXERCISE 18-10-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. Write a declaration for each variable/function:\n\n");
+
+	printf(COLOR_CYAN "(a) p is a pointer to a function whith a character pointer argument that returns a character pointer.\n");
+	printf(COLOR_MAGENTA "Declaration: " COLOR_RESET "char *(*p)(char *)\n\n");
+
+	printf(COLOR_CYAN "(b) f is a function with two arguments: p, a pointer to a struct with tag t, and n, a long integer.\n"
+			          "    f returns a pointer to a function that has no arguments and returns nothing.\n");
+	printf(COLOR_MAGENTA "Declaration: " COLOR_RESET "void (*f(struct t *p, long int n))(void)\n\n");
+
+	printf(COLOR_CYAN "(c) a is an array of four pointers to functions that have no arguments and return nothing.\n"
+			          "    The elements of a initially point to functions named insert, search, update, and print.\n");
+	printf(COLOR_MAGENTA "Declaration: " COLOR_RESET "void (*a[4])(void) = {insert,\n"
+			             "                                                  search,\n"
+						 "                                                  update,\n"
+						 "                                                  print};\n\n");
+
+	printf(COLOR_CYAN "(d) b is an array with 10 pointers to functions with two int arguments that return structures with tag t.\n");
+	printf(COLOR_MAGENTA "Declaration: " COLOR_RESET "struct t (*b[10])(int, int)\n\n");
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
