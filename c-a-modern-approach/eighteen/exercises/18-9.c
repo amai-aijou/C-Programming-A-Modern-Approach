ee1c4ad2e2026ddf0db3329a61f95ec8a42c11d9
amai-aijou <joseph.russ.olson@gmail.com>
2026-06-25 20:52:45 -0500

    18-9 Exercise - COMPLETE!

--- c-a-modern-approach/eighteen/exercises/18-9.c
+++ c-a-modern-approach/eighteen/exercises/18-9.c
@@ -15,34 +15,27 @@
 #define COLOR_RED "\033[1;31m"
 #define COLOR_GREEN "\033[1;32m"
 #define COLOR_YELLOW "\033[1;33m"
+#define COLOR_BLUE "\033[1;34m"
 #define COLOR_MAGENTA "\033[1;35m"
 #define COLOR_CYAN "\033[1;36m"
 #define COLOR_RESET "\033[1;0m"
 
-// (a) char (*x[10])(int);
-typedef char Fcn_a(int);
-typedef Fcn_a *Fcn_ptr_a;
-typedef Fcn_ptr_a Fcn_ptr_array_a[10];
-typedef Fcn_ptr_array_a v;
 
-// (b) int (*x(int))[5];
-typedef int Fcn_ptr_b[5];
-typedef Fcn_ptr_b *Fcn_int_b;
-typedef Fcn_int_b Fcn_b(int);
-typedef Fcn_b x;
+/*
+(COLOR_CYAN "(c) float *(*x(void))(int);\n");
 
-// (c) float *(*x(void))(int);
-typedef float *Fcn_ptr_c(int);
-typedef Fcn_ptr_c *Fcn_void_c;
-typedef Fcn_void_c Fcn_c(void);
-typedef Fcn_c x;
 
-// (d) void (*x(int, void (*y)(int)))(int);\n");
+// (d) void (*x(int, void (*y)(int)))(int);
 
-// build y first
-typedef  Fcn_y;
-typedef Fcn_y y;
 
+
+// then plug in: void (*x(int, Ptr_y))(int);
+typedef void Fcn_ptr_void_x(int);
+typedef Fcn_ptr_void_x *Fcn_ptr_x;
+typedef Fcn_ptr_x Fcn_x(int, Ptr_y);
+Fcn_x x;
+
+*/
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                 ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
@@ -52,17 +45,37 @@ int main(void) {
 	printf(COLOR_MAGENTA "Q. Fully describe the type of x in each declaration:\n\n");
 
 	printf(COLOR_CYAN "(a) char (*x[10])(int);\n");
-	printf(COLOR_MAGENTA "x is: " COLOR_RESET "an array of ten function pointers that have an int argument and return a char\n\n");
+	printf(COLOR_MAGENTA "  x is: \n" COLOR_RESET "    an array of ten function pointers that have an int argument and return a char\n");
+	printf(COLOR_MAGENTA "  typedef:\n" COLOR_RESET
+	"      typedef char Fcn(int);\n"
+	"      typedef Fcn *Fcn_ptr;\n"
+	"      typedef Fcn_ptr Fcn_ptr_array[10];\n"
+	"      Fcn_ptr_array x;\n\n");
 
 	printf(COLOR_CYAN "(b) int (*x(int))[5];\n");
-	printf(COLOR_MAGENTA "x is: " COLOR_RESET "a function with an int argument and returns an pointer to an array of five ints\n\n");
+	printf(COLOR_MAGENTA "  x is: \n" COLOR_RESET "    a function with an int argument and returns an pointer to an array of five ints\n");
+	printf(COLOR_MAGENTA "  typedef:\n" COLOR_RESET
+	"      typedef int Fcn_ptr_array[5];\n"
+	"      typedef Fcn_ptr_array *Fcn_ptr;\n"
+	"      typedef Fcn_ptr Fcn_int(int);\n"
+	"      Fcn_int x;\n\n");
 
 	printf(COLOR_CYAN "(c) float *(*x(void))(int);\n");
-	printf(COLOR_MAGENTA "x is: " COLOR_RESET "a function with a void parameter that returns a pointer to a function with an int parameter and a float return type \n\n");
+	printf(COLOR_MAGENTA "  x is: \n" COLOR_RESET "    a function with a void parameter that returns a pointer to a function with an int parameter and a float return type \n");
+	printf(COLOR_MAGENTA "  typedef:\n" COLOR_RESET
+	"      typedef float Fcn_ptr_float(int);\n"
+	"      typedef Fcn_ptr_float *Fcn_ptr;\n"
+	"      typedef Fcn_ptr Fcn_void(void);\n"
+	"      Fcn_void x;\n\n");
 
 	printf(COLOR_CYAN "(d) void (*x(int, void (*y)(int)))(int);\n");
-	printf(COLOR_MAGENTA "x is: " COLOR_RESET "a function with an int parameter and a void parameter y (a function pointer that takes an int),\n"
-		                 "which returns a void function pointer that takes an int\n\n");
+	printf(COLOR_MAGENTA "x is: \n" COLOR_RESET "    a function with an int parameter and a void parameter y (a function pointer that takes an int),\n"
+		                 "    which returns a void function pointer that takes an int\n");
+	printf(COLOR_MAGENTA "  typedef:\n" COLOR_BLUE
+	"      // build y first\n" COLOR_RESET
+	"      typedef void Fcn_ptr_y(int);\n"
+	"      typedef Fcn_ptr_y *Ptr_y;\n"
+	"      Ptr_y y;\n\n");
 
 	return 0;
 }
