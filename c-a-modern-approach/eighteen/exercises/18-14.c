777a032c1b6e75c6f6d36ba0732a228ac823cc89
amai-aijou <joseph.russ.olson@gmail.com>
2026-06-30 19:42:00 -0500

    18-14 Exercise

---
+++ c-a-modern-approach/eighteen/exercises/18-14.c
@@ -0,0 +1,48 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 18-14.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Tue Jun 30 07:33:55 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	printf("-------------------EXERCISE 18-14-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. Which of these variables can be initialized?\n");
+
+	printf(COLOR_MAGENTA "(a) array\n"
+		   COLOR_CYAN    "    Init.: " COLOR_RESET "Legal\n\n");
+
+	printf(COLOR_MAGENTA "(b) enum.\n"
+		   COLOR_CYAN    "    Init.: " COLOR_RESET "Legal\n\n");
+
+	printf(COLOR_MAGENTA "(c) struct\n"
+		   COLOR_CYAN    "    Init.: " COLOR_RESET "Legal\n\n");
+
+	printf(COLOR_MAGENTA "(d) union\n"
+		   COLOR_CYAN    "    Init.: " COLOR_RESET "Legal (in C99)\n"
+		   COLOR_CYAN    "     Note: " COLOR_RESET "Requires a Designated Initializer, which is part of the C99 standard\n\n");
+
+	return 0;
+}
