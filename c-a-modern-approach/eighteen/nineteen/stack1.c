c422b8404b28403f4d9773c776530ed662c408e8
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-03 18:46:53 -0500

    Chapter 19: Notes & Programs

---
+++ c-a-modern-approach/eighteen/nineteen/stack1.c
@@ -0,0 +1,56 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: stack1.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Fri Jul  3 04:03:36 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+#define STACK_SIZE 100
+
+static int contents[STACK_SIZE];
+static int top = 0;
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+static void terminate(const char *message) {
+
+	printf("%s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+void make_empty(void) {
+
+	top = 0;
+}
+
+bool is_empty(void) {
+	return top == 0;
+}
+
+bool is_full(void) {
+	return top == STACK_SIZE;
+}
+
+void push(int i) {
+	if (is_full()) {
+		terminate("Error in push: stack is full.");
+	}
+	contents[top++] = i;
+}
+
+int pop(void) {
+	if (is_empty()) {
+		terminate("Error in push: stack is empty.");
+	}
+	return contents[--top];
+}
---
+++ c-a-modern-approach/eighteen/nineteen/stack2.c
@@ -0,0 +1,80 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: stack2.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Fri Jul  3 06:39:24 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+struct node {
+	int data;
+	struct node *next;
+};
+
+static struct node *top = NULL;
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+static void terminate(const char *message) {
+	printf("%s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+void make_empty(void) {
+	while (!is_empty()) {
+		pop();
+	}
+}
+
+bool is_empty(void) {
+	return top == NULL;
+}
+
+bool is_full(void){
+	return false;
+}
+
+void push(int i) {
+	struct node *new_node = malloc(sizeof(struct node));
+	if (new_node == NULL) {
+		terminate("Error in push: stack is full.");
+	}
+	new_node->data = i;
+	new_node->next = top;
+	top = new_node;
+}
+
+int pop(void) {
+	struct node *old_top;
+	int i;
+
+	if (is_empty()) {
+		terminate("Error in pop: stack is empty.");
+	}
+
+	old_top = top;
+	i = top->data;
+	top = top->next;
+	free(old_top);
+	return i;
+}
