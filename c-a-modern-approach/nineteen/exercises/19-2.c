6704f7cd96a6b5063283ae9dd63f89177bc5152c
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-07 14:28:01 -0500

    Ch. 19: Exercise 2

--- c-a-modern-approach/nineteen/exercises/19-2.c
+++ c-a-modern-approach/nineteen/exercises/19-2.c
@@ -3,7 +3,7 @@
    ❤︎︎࣪ Name: 19-2.c
    ❤︎︎࣪ Purpose: 
    ❤︎︎࣪ Author: amai-aijou
-   ❤︎︎࣪ Date: Mon Jul  6 11:50:19 PM CDT 2026
+   ❤︎︎࣪ Date: Fri Jul  3 06:39:24 PM CDT 2026
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
                                                                 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -11,6 +11,18 @@
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+#define PUBLIC // Empty define statement as public is just, standard
+#define PRIVATE static
+
+struct node {
+	int data;
+	struct node *next;
+};
+
+static struct node *top = NULL;
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                   ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
@@ -26,4 +38,46 @@ int main(void) {
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                    ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+PRIVATE void terminate(const char *message) {
+	printf("%s\n", message);
+	exit(EXIT_FAILURE);
+}
 
+PUBLIC void make_empty(void) {
+	while (!is_empty()) {
+		pop();
+	}
+}
+
+PUBLIC bool is_empty(void) {
+	return top == NULL;
+}
+
+PUBLIC bool is_full(void){
+	return false;
+}
+
+PUBLIC void push(int i) {
+	struct node *new_node = malloc(sizeof(struct node));
+	if (new_node == NULL) {
+		terminate("Error in push: stack is full.");
+	}
+	new_node->data = i;
+	new_node->next = top;
+	top = new_node;
+}
+
+PUBLIC int pop(void) {
+	struct node *old_top;
+	int i;
+
+	if (is_empty()) {
+		terminate("Error in pop: stack is empty.");
+	}
+
+	old_top = top;
+	i = top->data;
+	top = top->next;
+	free(old_top);
+	return i;
+}
