65b0365b39e0ed744cc640864ffd24134bd3974e
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-09 13:53:50 -0500

    Ch 19: Exercise 4

--- c-a-modern-approach/nineteen/exercises/19-4a.c
+++ c-a-modern-approach/nineteen/exercises/19-4a.c
@@ -1,9 +1,9 @@
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                  ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
-   ❤︎︎࣪ Name: 18-15.c
+   ❤︎︎࣪ Name: 19-4a.c
    ❤︎︎࣪ Purpose: 
    ❤︎︎࣪ Author: amai-aijou
-   ❤︎︎࣪ Date: Wed Jul  1 09:45:11 PM CDT 2026
+   ❤︎︎࣪ Date: Thu Jul  9 01:16:06 PM CDT 2026
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
                                                                 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -11,13 +11,14 @@
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 
 #include <stdio.h>
+#include <stdlib.h>
 
-#define COLOR_RED "\033[1;31m"
-#define COLOR_GREEN "\033[1;32m"
-#define COLOR_YELLOW "\033[1;33m"
-#define COLOR_MAGENTA "\033[1;35m"
-#define COLOR_CYAN "\033[1;36m"
-#define COLOR_RESET "\033[1;0m"
+#define STACK_SIZE 100
+
+typedef struct {
+	int contents[STACK_SIZE];
+	int top;
+} Stack;
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                   ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
@@ -28,22 +29,10 @@
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 int main(void) {
 
-	printf("-------------------EXERCISE 18-15-------------------\n\n");
-	printf(COLOR_MAGENTA "Q. Which variable property determines whether the compiler provides it a default initial value?\n\n");
-
-	printf(COLOR_MAGENTA "(a) Storage Duration\n"
-		   COLOR_CYAN    "    Determines Initial Default Value (if static) \n"
-		   COLOR_RESET   "    auto duration has no Default value and cannot be predicted, but static will have default\n"
-		                 "    values depending on type. 0 for integer, 0.0 for float, and a NULL pointer for pointers\n\n");
-
-	printf(COLOR_MAGENTA "(b) Scope\n"
-		   COLOR_CYAN   "    No Default value and cannot be predicted!\n\n");
-
-	printf(COLOR_MAGENTA "(c) Linkage\n"
-		   COLOR_CYAN   "    No Default value and cannot be predicted!\n\n");
-
-	printf(COLOR_MAGENTA "(b) Type\n"
-		   COLOR_CYAN   "    No Default value and cannot be predicted!\n\n");
-
-	return 0;
+	Stack s1, s2;
 }
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
--- c-a-modern-approach/nineteen/exercises/19-4b.h
+++ c-a-modern-approach/nineteen/exercises/19-4b.h
@@ -1,29 +1,34 @@
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                  ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
-   ❤︎︎࣪ Name: test.c
+   ❤︎︎࣪ Name: 19-4b.h
    ❤︎︎࣪ Purpose: 
    ❤︎︎࣪ Author: amai-aijou
-   ❤︎︎࣪ Date: Wed Jul  8 06:26:38 PM CDT 2026
+   ❤︎︎࣪ Date: Thu Jul  9 01:31:31 PM CDT 2026
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
                                                                 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                       ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 
+#ifndef STACKLL-H
+#define STACKLL-H
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct stack_type *Stack;
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                   ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+void terminate(const char *message);
+Stack create(void);
+void destroy(Stack s);
+void make_empty(Stack s);
+bool is_empty(Stack s);
+bool is_full(Stack s);
+void push(Stack s, Item i);
+Item pop(stack s);
 
-/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
-  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-int main(void) {
-
-}
-
-/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
-  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-
+#endif
