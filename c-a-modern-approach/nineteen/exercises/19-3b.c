f368a231e06d41ef944dc62cf1676b7e5f038250
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-08 21:35:25 -0500

    Ch 19: Exercise 3b (I got guilty and finished it)

--- c-a-modern-approach/nineteen/exercises/19-3b.c
+++ c-a-modern-approach/nineteen/exercises/19-3b.c
@@ -14,15 +14,15 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define STACK_SIZE 100
-
 typedef int Item;
 
-static Item contents[STACK_SIZE];
-static Item top = 0;
-static Item next = 0;
-static int size = 0;
+struct node {
+	Item data;
+	struct node *next;
+};
 
+// Define head and tail of linked list queue
+static struct node *top = NULL, *tail = NULL;;
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                    ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
@@ -30,34 +30,63 @@ static int size = 0;
 // Inserts an item at the end of the queue
 void Insert(Item x) {
 
-	contents[top] = x;;
-	top = (top + 1) % STACK_SIZE; // I got this from AI, it's actually sleek. Credit where credit is due!
-	size++;
+	struct node *new_node = malloc(sizeof(struct node));
+	if (new_node == NULL) {
+		printf("Error on insert: queue is full\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (tail == NULL) {
+		top = new_node;
+		new_node->data = x;
+		new_node->next = NULL;
+		tail = new_node;
+		return;
+	}
+
+	new_node->data = x;
+	new_node->next = tail->next;
+	tail->next = new_node;
+	tail = new_node;
+
 }
 
 // Removes an item from the beginning of the queue
 Item Remove(void) {
-	Item num = contents[next];
 
-	next = (next + 1) % STACK_SIZE;
-	size--;
+	if (is_empty()) {
+		printf("Error: Stack is empty\n");
+		exit(EXIT_FAILURE);
+	}
+
+	struct node *trash;
+	Item num = top->data;
+
+	trash = top;
+	top = top->next;
+	free(trash);
+
+	if (top == NULL) {
+		tail = NULL;
+	}
+
 	return num;
 }
 
 // Returns the first item in the queue (without changing anything)
 Item return_first(void) {
 
-	return contents[next];
+	return top->data;
 }
 
 // Returns the last item in the queue (without changing anything)
 Item return_last(void) {
 
-	return contents[top - 1];
+	return tail->data;
 }
 
 // Checks whether the queue is empty
 bool is_empty(void) {
 	
-	return size == 0;
+	return top == NULL;
 }
