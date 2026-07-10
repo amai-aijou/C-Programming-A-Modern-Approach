a5312281ce5a1e828f58582f0824c8ae483de5e8
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-10 13:18:12 -0500

    Ch 19: Exercise 5

---
+++ c-a-modern-approach/nineteen/exercises/19-5.h
@@ -0,0 +1,37 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 19-5.h
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Mon Jul  6 11:35:01 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#ifndef QUEUE_H
+#define QUEUE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef int Item;
+
+typedef struct {
+	Item contents[STACK_SIZE];
+	Item top;
+	Item tail;
+	int size;
+} Queue;
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+void Insert(Queue *q, Item x);
+Item Remove(Queue *q);
+Item return_first(Queue *q);
+Item return_last(Queue *q);
+bool is_empty(Queue *q);
+
+#endif
