40762c2a5df7f4cf0c4c597ff4fd880929d516a5
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-11 13:44:46 -0500

    Ch 19: Exercise 6

--- c-a-modern-approach/nineteen/exercises/19-6/19-6a.c
+++ c-a-modern-approach/nineteen/exercises/19-6/19-6a.c
@@ -1,9 +1,9 @@
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                  ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
-   ❤︎︎࣪ Name: 19-4b.c.c
+   ❤︎︎࣪ Name: stackADT.c
    ❤︎︎࣪ Purpose: 
    ❤︎︎࣪ Author: amai-aijou
-   ❤︎︎࣪ Date: Thu Jul  9 01:43:25 PM CDT 2026
+   ❤︎︎࣪ Date: Sat Jul  4 04:42:37 PM CDT 2026
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
                                                                 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -11,19 +11,69 @@
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "stackADT.h"
 
-/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
-  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+#define STACK_SIZE 100
 
-/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
-  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-int main(void) {
-
-}
+struct stack_type {
+	Item contents[STACK_SIZE];
+	int top;
+};
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                    ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+// Looks at the top item of the stack without modifying it. Returns the item it "looked" at
+Item peek(Stack s) {
 
+	return s->contents[s->top-1];
+}
+
+static void terminate(const char *message) {
+
+	printf("%s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+Stack create(void) {
+
+	Stack s = malloc(sizeof(struct stack_type));
+	if (s == NULL) {
+		terminate("Error in create: stack could not be created.");
+	}
+	s->top = 0;
+	return s;
+}
+
+void destroy(Stack s) {
+	free(s);
+}
+
+void make_empty(Stack s) {
+	s->top = 0;
+}
+
+bool is_empty(Stack s) {
+	return s->top == 0;
+}
+
+bool is_full(Stack s) {
+	return s->top == STACK_SIZE;
+}
+
+void push(Stack s, Item i) {
+	if (is_full(s)) {
+		terminate("Error in push: stack is full.");
+	}
+	s->contents[s->top++] = i;
+}
+
+Item pop(Stack s) {
+
+	if (is_empty(s)) {
+		terminate("Error in push: stack is empty.");
+	}
+
+	return	s->contents[--s->top];
+}
---
+++ c-a-modern-approach/nineteen/exercises/19-6/19-6c.c
@@ -0,0 +1,100 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: stackADT.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sat Jul  4 04:42:37 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stackADT.h"
+
+struct node {
+	Item data;
+	struct node *next;
+};
+
+struct stack_type {
+	struct node *top;
+};
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+Item peek(Stack s) {
+
+	if (is_empty(s)) {
+		terminate("Error in peek: stack is empty.");
+	}
+	return s->top->data;
+}
+
+static void terminate(const char *message) {
+
+	printf("%s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+Stack create(void) {
+
+	Stack s = malloc(sizeof(struct stack_type));
+	if (s == NULL) {
+		terminate("Error in create: stack could not be created.");
+	}
+	s->top = NULL;
+	return s;
+}
+
+void destroy(Stack s) {
+
+	make_empty(s);
+	free(s);
+}
+
+void make_empty(Stack s) {
+
+	while (!is_empty(s)) {
+		pop(s);
+	}
+}
+
+bool is_empty(Stack s) {
+	return s->top == NULL;
+}
+
+bool is_full(Stack s) {
+	return false;
+}
+
+void push(Stack s, Item i) {
+
+	struct node *new_node = malloc(sizeof(struct node));
+	if (new_node == NULL) {
+		terminate("Error in push: stack is full.");
+	}
+	new_node->data = i;
+	new_node->next = s->top;
+	s->top = new_node;
+}
+
+Item pop(Stack s) {
+
+	struct node *old_top;
+	Item i;
+
+	if (is_empty(s)) {
+		terminate("Error in push: stack is empty.");
+	}
+
+	old_top = s->top;
+	i = old_top->data;
+	s->top = old_top->next;
+	free(old_top);
+
+	return i;
+}
