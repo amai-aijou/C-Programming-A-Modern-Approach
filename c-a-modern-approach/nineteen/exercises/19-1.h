c628b2f9093d7dbf85b24a94e1f20b424cc622d1
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-19 23:59:00 -0500

    Ch. 20: Notes (Forgot to add last night, spent two hours yesterday)

--- c-a-modern-approach/nineteen/exercises/19-1.h
+++ c-a-modern-approach/nineteen/exercises/19-1.h
@@ -18,13 +18,20 @@
 
 typedef int Item;
 
+typedef struct queue_type *Queue;
+
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                   ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-void Insert(Item x);
-Item Remove(void);
-Item return_first(void);
-Item return_last(void);
-bool is_empty(void);
+static void terminate(char *message);
+Queue create(void);
+void destroy(Queue q);
+void make_empty(Queue q);
+bool is_empty(Queue q);
+bool is_full(Queue q);
+void Insert(Item x, Queue q);
+Item Remove(Queue q);
+Item return_first(Queue q);
+Item return_last(Queue q);
 
 #endif
