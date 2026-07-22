e0650d34c30e6eed8e331eb6e63086ac6188e847
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-22 16:16:13 -0500

    Ch. 20: Exercise 2

--- c-a-modern-approach/twenty/exercises/20-2.c
+++ c-a-modern-approach/twenty/exercises/20-2.c
@@ -1,9 +1,9 @@
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                  ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
-   ❤︎︎࣪ Name: 18-15.c.c
+   ❤︎︎࣪ Name: 20-2
    ❤︎︎࣪ Purpose: 
    ❤︎︎࣪ Author: amai-aijou
-   ❤︎︎࣪ Date: Wed Jul  1 09:45:15 PM CDT 2026
+   ❤︎︎࣪ Date: Wed Jul 22 02:26:59 PM CDT 2026
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
                                                                 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
@@ -11,6 +11,12 @@
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 
 #include <stdio.h>
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
 
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                   ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
@@ -21,9 +27,26 @@
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 int main(void) {
 
+	unsigned short i = 16, j = 239;
+
+	i ^= 0x10;
+	j ^= 0x10;
+
+	printf("-------------------EXERCISE 20-2-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. How can you toggle a bit? Explain by toggling bit 4 of i \n\n");
+
+	printf(COLOR_CYAN    "A bitmask can accomplish this with the Exclusive OR (^=) and a bitmask of '0001 0000', aka '16', aka '0x10 \n");
+	printf(COLOR_RESET   "       16 ^= 0x10 (16)\n"
+			             "0001 0000 ^= 0001 0000\n"
+					     "0000 0000\n\n"
+
+						 "      239 ^= 0x10\n"
+						 "1110 1111 ^= 0001 0000\n"
+						 "1111 1111\n\n");
+	printf(COLOR_MAGENTA "Program Output: \n");
+	printf(COLOR_CYAN	 "16  ^= 0x10: " COLOR_RESET "%d\n", i);
+	printf(COLOR_CYAN	 "237 ^= 0x10: " COLOR_RESET "%d\n", j);
+	printf("\n");
+
+	return 0;
 }
-
-/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
-  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
-
