e06eacf3cca3036bdcd1583facff70929a139594
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-24 18:14:55 -0500

    Ch. 20: Ex. 4

--- c-a-modern-approach/twenty/exercises/20-5.c
+++ c-a-modern-approach/twenty/exercises/20-5.c
@@ -1,6 +1,6 @@
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                  ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
-   ❤︎︎࣪ Name: 20-4
+   ❤︎︎࣪ Name: 20-5.c
    ❤︎︎࣪ Purpose: 
    ❤︎︎࣪ Author: amai-aijou
    ❤︎︎࣪ Date: Thu Jul 23 09:22:39 PM CDT 2026
@@ -18,6 +18,8 @@
 #define COLOR_CYAN "\033[1;36m"
 #define COLOR_RESET "\033[1;0m"
 
+#define MK_COLOR(x,y,z) ((x) | (y << 8) | (z << 16))
+
 /*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
                   ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
@@ -27,14 +29,18 @@
   ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
 int main(void) {
 
+	long int color;
+
 	printf("-------------------EXERCISE 20-4-------------------\n\n");
 	printf(COLOR_MAGENTA "Q. Create macro MK_COLOR and have it return a long with three parameters: REd, GREEN, BLUE\n"
 			             "   These parameters will take up the last three bytes.\n"
 						 "   Note: Remember x64 islittle-endian, so Red will be the last byte!\n\n");
 
-	printf(COLOR_CYAN    "(a)\n"
-	printf(COLOR_RESET   " Guess: \n"
-					     "Output: \n");
+	printf(COLOR_CYAN    "(a)\n");
+	printf(COLOR_RESET   " Guess: #define MK_COLOR(x,y,z) ((x) |(y << 8) | (z << 16))\n"
+					     "Output: ");
+	color = MK_COLOR(0,0,0);
+	printf("%d\n", color);
 
 	printf(COLOR_MAGENTA "(b)\n"
 		   COLOR_CYAN    "    Status: " COLOR_RESET "Legal/Illegal/True/False\n"
