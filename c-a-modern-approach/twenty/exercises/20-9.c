cbde2620ae9aed9522fbdc961f83acf2f2009cf0
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-28 23:52:38 -0500

    Ch. 20: Ex. 9 Pt. 1 (a)

---
+++ c-a-modern-approach/twenty/exercises/20-9.c
@@ -0,0 +1,74 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 20-9.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Tue Jul 28 11:06:15 AM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int count_ones(unsigned char ch);
+int count_ones_loopless(unsigned char ch);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	unsigned char nChar;
+
+	printf("-------------------EXERCISE 20-9-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. Write this function, both with and without using a loop! \n\n");
+	
+	printf(COLOR_CYAN "Enter a number, please: " COLOR_RESET);
+	scanf("%c", &nChar);
+
+	printf(COLOR_MAGENTA "(a) int count_ones(unsigned char ch);\n");
+	printf(COLOR_CYAN    "    output :" COLOR_RESET "There are %d ones in %c (ASCII: %d)\n\n", count_ones(nChar), nChar, nChar);
+
+	printf(COLOR_MAGENTA "int count_ones_loopless(unsigned char ch);\n");
+	printf(COLOR_CYAN    "(b)  No loop!: \n");
+	printf(COLOR_RESET   "   \n\n");
+
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int count_ones(unsigned char ch) {
+
+	int i, ones = 0, digit;
+
+	printf(COLOR_RESET "sizeof unsigned char: %d\n", (sizeof(unsigned char) * 8));
+
+	for (i = 0; i < (sizeof(unsigned char) * 8); i++) {
+		if ((digit = ch >> (i + 1 - 1)) & ~(~0 << 1)) {
+			ones++;
+		}
+		printf("digit: %d\n", digit);
+	}
+
+	return ones;
+}
+
+int count_ones_loopless(unsigned char ch) {
+
+
+}
