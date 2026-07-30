c3d39024250144c7a4b9e62e7fe87ed11fa10a36
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-29 22:16:30 -0500

    Ch. 20: Ex. 9 (b)

--- c-a-modern-approach/twenty/exercises/20-9.c
+++ c-a-modern-approach/twenty/exercises/20-9.c
@@ -36,15 +36,23 @@ int main(void) {
 	printf(COLOR_MAGENTA "Q. Write this function, both with and without using a loop! \n\n");
 	
 	printf(COLOR_CYAN "Enter a number, please: " COLOR_RESET);
-	scanf("%c", &nChar);
+	scanf("%d", &nChar);
+	printf("\n");
 
 	printf(COLOR_MAGENTA "(a) int count_ones(unsigned char ch);\n");
-	printf(COLOR_CYAN    "    output :" COLOR_RESET "There are %d ones in %c (ASCII: %d)\n\n", count_ones(nChar), nChar, nChar);
+	printf(COLOR_CYAN    "    output :" COLOR_RESET "There are %d ones in %d (ASCII: %d)\n\n", count_ones(nChar), nChar, nChar);
 
-	printf(COLOR_MAGENTA "int count_ones_loopless(unsigned char ch);\n");
-	printf(COLOR_CYAN    "(b)  No loop!: \n");
-	printf(COLOR_RESET   "   \n\n");
+	printf(COLOR_MAGENTA "(b) int count_ones_loopless(unsigned char ch);\n");
+	printf(COLOR_CYAN    "    output :" COLOR_RESET "There are %d ones in %d (ASCII: %d)\n\n", count_ones_loopless(nChar), nChar, nChar);
 
+	printf(COLOR_RESET   "    sizeof unsigned char: %d\n\n", (sizeof(unsigned char) * 8));
+	printf(              "    The loopless function depends on turning the eight 1-bit numbers...into four 2-bit...\n"
+			             "    into two 4-bit...and finally, into one eight-bit number, which is the same size as the 8-bit char 'container' it sits in.\n"
+						 "    As it is a single number, it can be read as a single decimal via %%d\n\n");
+
+	printf(              "    You can double the 'size' of a number by making a mask twice its size (for 2-bit, this was 01010101...for 4-bit, 00110011).\n"
+			             "    You then apply that mask to the number...then, to that number shifted by the bitrate of the number (1, 2, 4, for 2/4/8-bit).\n"
+						 "    Finally, you add those two together through binary addition, and use it as the input for the next\n\n");
 
 	return 0;
 }
@@ -56,13 +64,11 @@ int count_ones(unsigned char ch) {
 
 	int i, ones = 0, digit;
 
-	printf(COLOR_RESET "sizeof unsigned char: %d\n", (sizeof(unsigned char) * 8));
 
 	for (i = 0; i < (sizeof(unsigned char) * 8); i++) {
 		if ((digit = ch >> (i + 1 - 1)) & ~(~0 << 1)) {
 			ones++;
 		}
-		printf("digit: %d\n", digit);
 	}
 
 	return ones;
@@ -70,5 +76,67 @@ int count_ones(unsigned char ch) {
 
 int count_ones_loopless(unsigned char ch) {
 
+	unsigned char twoBit, fourBit, eightBit;
 
+	twoBit = (ch & 0x55) + ((ch >> 1) & 0x55);
+	fourBit = (twoBit & 0x33) + ((twoBit >> 2) & 0x33);
+	eightBit = (fourBit & 0xF) + ((fourBit >> 4) & 0xF);
+
+	return eightBit;
 }
+
+/*
+1-bit:
+	10 11 01 10
+
+2-bit mask:
+	01 01 10 11 ( ch >> 1)
+   &01 01 01 01 (0x55)
+    -----------
+	01 01 00 01
+
+	10 11 01 10 (ch)
+   +01 01 01 01
+    -----------
+	00 01 01 00
+
+	01 01 00 01
+   +00 01 01 00
+    -----------
+	01 10 01 01 (2-bit number)
+
+4-bit mask:
+	00 01 10 01 (1-bit >> 2)
+   &00 11 00 11 (0x33)
+    -----------
+	00 01 00 01
+
+	01 10 01 01 (1-bit)
+   &00 11 00 11 (0x33)
+    -----------
+	00 10 00 01
+
+	00 01 00 01
+	00 10 00 01
+    -----------
+	00 11 00 10 (4-bit number)
+
+8-bit mask:
+	00 00 00 11 (2-bit >> 4) 
+   &00 00 11 11 (0xF)
+    -----------
+	00 00 00 11
+
+	00 11 00 10
+   &00 00 11 11 (0xF)
+    -----------
+	00 00 00 10
+
+	00 00 00 11
+	00 00 00 10
+    -----------
+	00 00 01 01 (8-bit number)
+
+The "unsigned char" is only eight bits; since the bit-rate now matches the size of the container,
+  this can be read as a normal human number! (in this example, "5")
+*/
