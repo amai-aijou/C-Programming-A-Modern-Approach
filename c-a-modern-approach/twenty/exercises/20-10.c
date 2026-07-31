c8287fd8e6f4f8235da2c34d4fbf68e4334d91f2
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-30 22:54:40 -0500

    Ch. 20: Ex. 10

---
+++ c-a-modern-approach/twenty/exercises/20-10.c
@@ -0,0 +1,70 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 20-10.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Thu Jul 30 06:35:40 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+#define VAR_SIZE (sizeof(unsigned int) * 8)
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+unsigned int reverse_bits(unsigned int n);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	unsigned int n;
+
+	printf("-------------------EXERCISE 20-10-------------------\n\n");
+	printf(COLOR_MAGENTA "Write this function: \n\n");
+
+	printf(COLOR_CYAN    "unsigned int reverse_bits(unsigned int n);\n");
+	printf(COLOR_RESET   "  Enter a number: ");
+	scanf("%d", &n);
+
+	printf("\nReversed bits: %u\n\n", reverse_bits(n));
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+unsigned int reverse_bits(unsigned int n) {
+
+	unsigned int reverse = 0;
+	unsigned int bitmask = 1;
+
+	int i;
+	int bitcounter = VAR_SIZE - 1;
+
+	for (i = 0; i < VAR_SIZE; i++) {
+
+		// create a bitmask...move it all the way to the right....then move it left by 'bitcounter'
+		reverse |= ((n & bitmask) >> i) << bitcounter;
+
+		// pull a single digit per iteration by moving mask left each loop
+		bitmask <<= 1;
+		bitcounter -= 1;
+	}
+
+	return reverse;
+}
