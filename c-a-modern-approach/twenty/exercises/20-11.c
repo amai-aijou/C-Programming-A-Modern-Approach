fd309a9128a1df3745e52c5a6871bc30acfba282
amai-aijou <joseph.russ.olson@gmail.com>
2026-07-31 22:42:53 -0500

    Ch. 20: Ex 11

---
+++ c-a-modern-approach/twenty/exercises/20-11.c
@@ -0,0 +1,61 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 20-11.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Fri Jul 31 02:54:03 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+#define SHIFT_BIT 1
+#define CTRL_BIT  2
+#define ALT_BIT   4
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	unsigned int key_code = 0;
+	unsigned int test;
+
+	printf("-------------------EXERCISE 20-11-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. Debug the following statement (and fix!) \n\n");
+
+	printf(COLOR_CYAN    "    if (key_code & (SHIFT_BIT | CTRL_BIT | ALT_BIT) == 0)\n"
+			             "        printf(\"No modifier keys pressed\\n\");\n\n");
+
+	printf(COLOR_RESET   "  Guess: Requires parentheses as == is higher precedence than & (crazy enough!)\n\n");
+
+	printf(COLOR_CYAN    "    if ((key_code & (SHIFT_BIT | CTRL_BIT | ALT_BIT)) == 0)\n"
+			             "        printf(\"No modifier keys pressed\\n\");\n\n");
+
+
+	if ((key_code & (SHIFT_BIT | CTRL_BIT | ALT_BIT)) == 0) {
+		printf("No modifier keys pressed\n\n");
+	} else {
+		printf("You are pressing a modifier!\n\n");
+	}
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
