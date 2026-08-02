babc9182763dfe15f208664d85629c9a974e2d81
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-01 20:58:19 -0500

    Ch. 20: Ex. 12

---
+++ c-a-modern-approach/twenty/exercises/20-12.c
@@ -0,0 +1,62 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 20-12.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sat Aug  1 01:06:58 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+unsigned short create_short(unsigned char high_byte,
+							unsigned char low_byte);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	unsigned char high = 2;
+	unsigned char low  = 1;
+
+	printf("-------------------EXERCISE 20-12-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. Why doesn't this function combine the two bytes via bit-packing? Fix it!\n\n");
+
+	printf(COLOR_CYAN    "unsigned short create_short(unsigned char high_byte,\n"
+			             "                            unsigned char low_byte) {\n\n"
+						 "    return high_byte << 8 + low_byte;\n"
+						 "}\n\n");
+
+	printf(COLOR_CYAN    "  What's wrong?: This will simply move high_bytes (8 + low_bytes) to the left. you have to move, then combine them\n"
+		   COLOR_RESET   "  Corrected Output:\n\n");
+	printf(				 "    Size of unsigned short: %d\n", (sizeof(unsigned short) * 8));
+	printf(				 "    Size of unsigned char: %d\n\n", (sizeof(unsigned char) * 8));
+
+	printf(				 "    create_short %d\n\n", create_short(high,low));
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+unsigned short create_short(unsigned char high_byte,
+							unsigned char low_byte) {
+
+
+return high_byte << 8 | low_byte;
+}
