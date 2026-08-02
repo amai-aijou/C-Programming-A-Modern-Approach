c87402c8ae4d10c1c0b207ec3644346324ace0e9
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-02 13:46:31 -0500

    Ch. 20 Ex. 13

---
+++ c-a-modern-approach/twenty/exercises/20-13.c
@@ -0,0 +1,58 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 20-13.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sun Aug  2 01:08:04 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	unsigned int n = 15;
+
+
+
+	printf("-------------------EXERCISE 20-13-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. What does this statement do? (hint: it may run more than once!)\n\n");
+
+	printf(COLOR_CYAN    "    unsigned int n;\n"
+			             "    n &= n - 1;\n\n");
+
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "n causes the right-most 1 bit to flip to a zero, ie:\n"
+			             "           15: 1111...1110...1100...1000\n\n");
+	printf(COLOR_CYAN    " Output: \n" COLOR_RESET); 
+
+	printf(              "        n: %d\n", n);
+	n &= n - 1;
+	printf(              "   Run 1x: %d\n", n);
+	n &= n - 1;
+	printf(              "   Run 2x: %d\n", n);
+	n &= n - 1;
+	printf(              "   Run 2x: %d\n", n);
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
