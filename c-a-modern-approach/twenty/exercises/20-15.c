df48a995dd4699822965bfc87c5324f64fe96b9e
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-04 16:58:24 -0500

    Ch. 20: Ex. 15

---
+++ c-a-modern-approach/twenty/exercises/20-15.c
@@ -0,0 +1,68 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 20-15.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Tue Aug  4 04:23:40 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+struct {
+	int flag: 1;
+} s;
+
+struct {
+	unsigned int flag: 1;
+} t;
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	s.flag = 1;
+	t.flag = 1;
+
+	printf("-------------------EXERCISE 20-15-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. Say you have the following struct: \n\n");
+
+	printf(COLOR_CYAN    "    struct {\n"
+			             "        int flag: 1;\n"
+						 "    } s;\n\n");
+
+	printf(COLOR_MAGENTA "(a) Some compilers create a 1; others a -1. Why?\n");
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "The high-order bit can be treated as a sign-bit in some compilers\n"
+			             "            ...including GCC, which is little-endian on x64, so the right bit is the sign bit!\n");
+	printf(COLOR_CYAN    " Output: " COLOR_RESET); 
+	printf("%d\n\n", s.flag);
+
+	printf(COLOR_MAGENTA "(b) How can we prevent (a) from occurring?\n"
+		   COLOR_CYAN    "  Guess: " COLOR_RESET "We can't just add a blank bit to ensure the first bit is not used\n"
+		                 "           because the sign bit isn't actually part of the bitfield. it's before it...\n"
+						 "           so, we must use an unsigned int instead with GCC\n");
+	printf(COLOR_CYAN    " Output: " COLOR_RESET);
+	printf("%d\n\n", t.flag);
+
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
