a6d31b0318883e71990f475aa06564f496493cc2
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-07 20:18:13 -0500

    Ch. 21: Ex. 2

---
+++ c-a-modern-approach/twenty-one/exercises/21-2.c
@@ -0,0 +1,45 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: /home/jolson/amai-aijou/C-Programming-A-Modern-Approach/c-a-modern-approach/twenty-one/exercises/21-2.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Fri Aug  7 08:11:34 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	printf("-------------------EXERCISE 21-2-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. Find a standard header with a macro\n\n");
+
+	printf(COLOR_CYAN    "    // From: /usr/includemath.h\n"
+			             "    #define isnan(x) __builtin_isnan (x)\n\n");
+
+	printf(COLOR_RESET   "  Declares isnan() to be replaced by the newer __builtin_isnan() in the preprocessor. There are some messy if statements\n"
+			 			 "   that check whether you're on a new enough GCC build; if not, it uses a legacy version instead that is slower.\n\n");
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
