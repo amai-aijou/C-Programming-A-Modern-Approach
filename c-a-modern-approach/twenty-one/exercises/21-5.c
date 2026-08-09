3e209c38f024eae987040661a883f222af85bb97
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-09 15:30:47 -0500

    Ch. 21: Ex. 5

---
+++ c-a-modern-approach/twenty-one/exercises/21-5.c
@@ -0,0 +1,58 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 21-5.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sun Aug  9 03:17:42 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+#undef islower
+#define islower(c) ((c) >= 'a' && (c) <= 'z')
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	char lower = 'x';
+	char upper = 'X';
+
+	printf("-------------------EXERCISE 21-5-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. What makes this macro illegal?\n"
+		   COLOR_RESET   "  Note: This macro is from <ctype.h>\n\n");
+
+	printf(COLOR_CYAN    "  #define islower(c) ((c) >= 'a' && (c) <= 'z')\n");
+
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "It's illegal as <ctype.h> defines islower not only as a function, but as a macro:\n\n"
+	                     "         #define islower(c) __isctype((c), _ISlower)\n\n"
+						 "         As such, you must first #undef it, first, which erases the function. since this *is* just a function though,\n"
+						 "          you can use islower as a macro as long as <ctype.h> isn't included. It's not a globally reserved name!\n\n");
+
+	printf(COLOR_CYAN    "Proof:\n");
+	printf(              "Lower is lowercase: %d\n", islower(lower));
+	printf(              "Upper is uppercase: %d\n\n", islower(upper));
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
