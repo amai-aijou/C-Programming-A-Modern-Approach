c129ddd52966582f7400763435dd59e0d1f66305
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-10 19:34:31 -0500

    Ch. 21: Project 1

---
+++ c-a-modern-approach/twenty-one/projects/21-1.c
@@ -0,0 +1,65 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 21-1.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sun Aug  9 04:12:29 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stddef.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+struct s {
+	char a;
+	int b[2];
+	float c;
+} s1;
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	printf(COLOR_CYAN "\nSize of:\n" COLOR_RESET);
+	printf("  struct: %d\n", sizeof(s1) * 8);
+	printf("  char a: %d\n", sizeof(s1.a) * 8);
+	printf("  int b[0]: %d (single element)\n", sizeof(s1.b[0]) * 8);
+	printf("  int b[2]: %d (full array)\n", sizeof(s1.b) * 8);
+	printf("  float c: %d\n", sizeof(s1.c) * 8);
+
+	printf("\n---------------------------\n\n");
+
+	printf(COLOR_CYAN "Offsets:\n" COLOR_RESET);
+	printf("  Offset of s1.a: %d\n", (offsetof(struct s, a) * 8));
+	printf("  Offset of s1.b: %d\n", (offsetof(struct s, b) * 8));
+	printf("  Offset of s1.c: %d\n", (offsetof(struct s, c) * 8));
+
+	printf("\n---------------------------\n\n");
+
+	printf(COLOR_CYAN "Gaps:\n" COLOR_RESET);
+	printf("  There is a single gap of 24 bits, between a and b, as a is only 8 bits.\n"
+		   "   The rest have no gaps, as the compiler uses 32 bits as the storage unit size\n"
+		   "   Behind the scenes, the compiler has simply chosen to store every member in int-sized memory posiion!\n\n");
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
