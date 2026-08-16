faa2c54859cbc8d3d1654533bc0207788f57f653
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-16 15:17:59 -0500

    Ch 22: Ex 2

---
+++ c-a-modern-approach/twenty-two/exercises/22-2.c
@@ -0,0 +1,51 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 22-2.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sun Aug 16 03:13:03 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	printf("-------------------EXERCISE 22-2-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. Which mode of string is most likely to be passed to fopen in regards to:\n\n");
+
+	printf(COLOR_MAGENTA "(a) A db mgmt system opens a file containing records to be updated.\n");
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "r+\n\n");
+
+	printf(COLOR_MAGENTA "(b) A mail program opens a file of saved messages to add more messages\n");
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "a\n\n");
+
+	printf(COLOR_MAGENTA "(c) A graphics program opens a file containing a picture to be displayed\n");
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "rb\n\n");
+
+	printf(COLOR_MAGENTA "(d) An OS system command interpreter opens a \"shell script\" (or \"batch file\") containing commands to be executed\n");
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "r\n\n");
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
