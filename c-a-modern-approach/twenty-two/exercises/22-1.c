4b57f2a73e44e4e1c7022a69715df553d6a78f6f
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-16 15:12:49 -0500

    Ch 22. Exercise 1

---
+++ c-a-modern-approach/twenty-two/exercises/22-1.c
@@ -0,0 +1,53 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 22-1.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sun Aug 16 03:09:47 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	printf("-------------------EXERCISE 22-1-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. Are these files is more likely to contain text or binary?\n\n");
+
+	printf(COLOR_CYAN    "    unsigned int n;\n"
+			             "    n &= n - 1;\n\n");
+
+	printf(COLOR_MAGENTA "(a) A file of object code make by a C compiler (.0)\n");
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "Binary\n\n");
+
+	printf(COLOR_MAGENTA "(b) A program listing produced by a C compiler\n");
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "Text\n\n");
+
+	printf(COLOR_MAGENTA "(c) An email message sent\n");
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "Text\n\n");
+
+	printf(COLOR_MAGENTA "(d) A file containing a graphics image\n");
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "Binary\n\n");
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
