3435b091db4b67998f5e3fed87b34908611be5f2
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-17 18:06:17 -0500

    Ch 22 Ex. 3

---
+++ c-a-modern-approach/twenty-two/exercises/22-3.c
@@ -0,0 +1,63 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 22-3.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Mon Aug 17 04:32:57 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	char buffer[100];
+	char *filename = "test";
+	FILE *fp;
+
+	printf("-------------------EXERCISE 22-3-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. What's wrong with this program?\n\n");
+
+	printf(COLOR_CYAN    "    FILE *fp;\n\n"
+			             "    if (fp = fopen(filename, \"r\")) {\n"
+						 "        // read chars until EOF\n"
+						 "    }\n"
+						 "    fclose(fp);\n\n");
+
+
+
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "fclose(fp) must test fp isn't NULL; or, you can just ensure fopen isn't NULL and place it in the same block" COLOR_RESET "...\n\n");
+	printf(COLOR_CYAN    " Output: " COLOR_RESET); 
+
+
+	if (fp = fopen(filename, "r")) {
+		while(fgets(buffer, sizeof buffer, fp) != NULL) {
+			printf("%s", buffer);
+		}
+
+		fclose(fp);
+	} 
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
