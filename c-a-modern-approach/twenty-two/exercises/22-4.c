a221a9deb1fdbb07e568a313eaf0b2a7c24e3b5f
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-18 17:49:19 -0500

    Ch 22 Ex 4

---
+++ c-a-modern-approach/twenty-two/exercises/22-4.c
@@ -0,0 +1,52 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 22-4.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Tue Aug 18 05:44:23 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	printf("-------------------EXERCISE 22-4-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. How will each of these look display as %%#012.5g?\n\n");
+
+
+	printf(COLOR_MAGENTA "(a)83.7361\n");
+	printf(COLOR_CYAN    "  Output: " COLOR_RESET "%#012.5g\n\n", 83.7361 );
+
+	printf(COLOR_MAGENTA "(b) 29748.6607\n");
+	printf(COLOR_CYAN    "  Output: " COLOR_RESET "%#012.5g\n\n", 29748.6607);
+
+	printf(COLOR_MAGENTA "(c) 1054932234.0\n");
+	printf(COLOR_CYAN    "  Output: " COLOR_RESET "%#012.5g\n\n", 1054932234.0);
+
+	printf(COLOR_MAGENTA "(d) 0.0000235218\n");
+	printf(COLOR_CYAN    "  Output: " COLOR_RESET "%#012.5g\n\n", 0.0000235218);
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
