26ee48f52dd74fc17553632be0e22ed71c7b6cfe
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-22 17:59:25 -0500

    Ch. 22 Ex. 8

---
+++ c-a-modern-approach/twenty-two/exercises/22-8.c
@@ -0,0 +1,54 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 22-8.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sat Aug 22 05:32:53 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	char charTest[5];
+	char stringTest[5];
+
+	printf("-------------------EXERCISE 22-8-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. \"%%c\" is sometimes used to skip whitespace. some programmers use %%1s instead. Are they the same?\n\n");
+
+	printf(COLOR_CYAN    "  char: " COLOR_RESET); 
+	scanf(                               " %c", &charTest);
+
+	printf(COLOR_CYAN    "string: " COLOR_RESET); 
+	scanf(                               "%1s", &stringTest);
+
+	printf(COLOR_CYAN    "       output (char): " COLOR_RESET "%c\n", charTest[0]); 
+	printf(COLOR_CYAN    "     output (string): " COLOR_RESET "%s\n", stringTest); 
+
+	printf(COLOR_CYAN    "        Same?: " COLOR_RESET "They are very similar, however %%1s can only be used on a string or char array, as it appends \\0 at the end.\n"
+		                 "                This reduces the flexibility. Also, you can use %%c for a string, though neither of these sound like a great idea.\n\n");
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
