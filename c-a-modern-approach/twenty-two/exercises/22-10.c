b993b5101e6d5cca4c9699c41c0edaa93f4ff832
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-24 18:26:02 -0500

    Ch. 22 Ex. 10

---
+++ c-a-modern-approach/twenty-two/exercises/22-10.c
@@ -0,0 +1,56 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: c-a-modern-approach/twenty-two/fcopy.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sat Aug 15 07:56:13 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(int argc, char *argv[]) {
+
+	FILE *source_fp, *dest_fp;
+	int ch;
+
+	if (argc != 3) {
+		fprintf(stderr, "usage: fcopy source dest\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((source_fp = fopen(argv[1], "rb")) == NULL) {
+		fprintf(stderr, "Can't open %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((dest_fp = fopen(argv[2], "wb")) == NULL) {
+		fprintf(stderr, "Can't open %s\n", argv[2]);
+		fclose(source_fp);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((ch = getc(source_fp)) != EOF) {
+		if (putc(ch, dest_fp) == EOF) {
+			fprintf(stderr, "Can't copy data!\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	fclose(source_fp);
+	fclose(dest_fp);
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
