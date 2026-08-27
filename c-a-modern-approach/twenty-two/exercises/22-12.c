8d3d2de15d243a7a9dbae3ad1b0d1017e162e093
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-26 19:16:05 -0500

    Ch. 22 Ex. 12

---
+++ c-a-modern-approach/twenty-two/exercises/22-12.c
@@ -0,0 +1,63 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 22-12.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Tue Aug 25 06:38:38 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int count_periods(const char *filename);
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	printf("-------------------EXERCISE 22-12-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. Where is the error in this function?\n\n");
+
+	printf(COLOR_CYAN    "    unsigned int n;\n"
+			             "    n &= n - 1;\n\n");
+
+	printf(COLOR_MAGENTA "(a)\n");
+	printf(COLOR_CYAN    "  Guess: " COLOR_RESET "fgetc() is being run twice in each loop. Capture its output in ch, and use that to check for periods!\n");
+	printf(COLOR_CYAN    " Output: " COLOR_RESET "%d\n\n", count_periods("sentence")); 
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int count_periods(const char *filename) {
+
+	FILE *fp;
+	int n = 0, ch;
+
+	if ((fp = fopen(filename, "r")) != NULL) {
+		while ((ch = fgetc(fp)) != EOF) {
+			if (ch == '.') {
+				n++;
+			}
+		}
+		fclose(fp);
+	}
+
+	return n;
+}
