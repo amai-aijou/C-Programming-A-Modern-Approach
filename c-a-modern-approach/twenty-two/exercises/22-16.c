dd21d063594263b520517e3d2f1736d5eb17fd53
amai-aijou <joseph.russ.olson@gmail.com>
2026-08-31 23:58:03 -0500

    Ch. 22: Ex. 16

---
+++ c-a-modern-approach/twenty-two/exercises/22-16.c
@@ -0,0 +1,59 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 22-16.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Mon Aug 31 01:43:40 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+
+#define COLOR_RED "\033[1;31m"
+#define COLOR_GREEN "\033[1;32m"
+#define COLOR_YELLOW "\033[1;33m"
+#define COLOR_MAGENTA "\033[1;35m"
+#define COLOR_CYAN "\033[1;36m"
+#define COLOR_RESET "\033[1;0m"
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(void) {
+
+	char *ranks[3] = {"test#989", "test#24,675", "#1,162,620yep"};
+	char *sales_rank[3];
+	int i;
+
+	printf("-------------------EXERCISE 22-16-------------------\n\n");
+	printf(COLOR_MAGENTA "Q. str is a string with \"sales rank\", preceded by the # symbol (other chars have preceded/follow this phrase).\n"
+			             "    the rank itself is a series of numbers, and may have commas in it, such as:\n\n");
+
+	printf(COLOR_CYAN    "    989\n"
+			             "    24,675\n"
+						 "    1,162,620\n\n");
+
+	printf(COLOR_MAGENTA "   Write a call of sscanf extracting the sales rank (without the # symbol!). Store it in the string variable sales_rank\n");
+	printf(COLOR_CYAN    " Output: " COLOR_RESET); 
+
+	for (i = 0; i < 3; i++) {
+		sscanf(ranks[i], "#%d", &sales_rank[i]);
+	}
+
+	for (i = 0; i < 3; i++) {
+		printf("%d\n", sales_rank[i]);
+	}
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
