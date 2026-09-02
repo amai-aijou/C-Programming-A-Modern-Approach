78e07188f89af791e6a78ebb397004e36f36ccb3
amai-aijou <joseph.russ.olson@gmail.com>
2026-09-01 19:30:54 -0500

    Ch. 22: Ex. 16

--- c-a-modern-approach/twenty-two/exercises/22-16.c
+++ c-a-modern-approach/twenty-two/exercises/22-16.c
@@ -29,7 +29,8 @@
 int main(void) {
 
 	char *ranks[3] = {"test#989", "test#24,675", "#1,162,620yep"};
-	char *sales_rank[3];
+	char *single_rank = "test#24,675";
+	char sales_rank[3][20];
 	int i;
 
 	printf("-------------------EXERCISE 22-16-------------------\n\n");
@@ -40,16 +41,24 @@ int main(void) {
 			             "    24,675\n"
 						 "    1,162,620\n\n");
 
-	printf(COLOR_MAGENTA "   Write a call of sscanf extracting the sales rank (without the # symbol!). Store it in the string variable sales_rank\n");
+	printf(COLOR_MAGENTA "   Write a call of sscanf extracting the sales rank (without the # symbol!). Store it in the string variable sales_rank\n\n");
 	printf(COLOR_CYAN    " Output: " COLOR_RESET); 
 
+	// Extracts sales_rank as an int whether it's preceded/followed by garbage, and regardless of number of commas
 	for (i = 0; i < 3; i++) {
-		sscanf(ranks[i], "#%d", &sales_rank[i]);
+		if ((sscanf(ranks[i], " %*[^#] # %[0-9,]", &sales_rank[i])) == 0) {
+
+			// Second call needed in case there is no garbage in the front
+			if((sscanf(ranks[i], "#%[0-9,]", &sales_rank[i])) == 0) {
+			}
+		}
 	}
 
+	printf("\n");
 	for (i = 0; i < 3; i++) {
-		printf("%d\n", sales_rank[i]);
+		printf("  %s\n", sales_rank[i]);
 	}
+	printf("\n");
 
 	return 0;
 }
