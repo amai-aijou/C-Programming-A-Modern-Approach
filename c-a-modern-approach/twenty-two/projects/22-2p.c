8eb41469e3fdae9aa6a2251f35ac99338027c369
amai-aijou <joseph.russ.olson@gmail.com>
2026-09-03 22:15:13 -0500

    Ch. 22: Project 2 WIP

---
+++ c-a-modern-approach/twenty-two/projects/22-2p.c
@@ -0,0 +1,61 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 22-2p.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Thu Sep  3 09:56:07 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_SIZE 500
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(int argc, char *argv[]) {
+
+	FILE *fp;
+
+	char line[MAX_SIZE];
+	char ch;
+
+	if ((fp = fopen(argv[1], "r+")) == NULL) {
+		printf("%s can't be opened\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+//	while ((line = fgets(s, MAX_SIZE, fp)) != EOF) {
+	while ((ch = getc(fp)) != EOF) {
+
+		if (isalpha(ch)) {
+			buffer[i] = toupper(ch);
+		}
+
+		if (putc(ch,fp) == EOF) {
+			fprintf(stderr, "Can't change character!\n");
+			exit(EXIT_FAILURE);
+		}
+
+		printf("DEBUG - Changing character: %c\n", ch);
+	}
+
+	fclose(fp);
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
