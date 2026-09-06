a2b7ce9ad063d1f2ccda9f21073900b2c31c0c00
amai-aijou <joseph.russ.olson@gmail.com>
2026-09-06 17:48:24 -0500

    Ch. 22: Program 5

---
+++ c-a-modern-approach/twenty-two/projects/22-5p.c
@@ -0,0 +1,55 @@
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                 ❤︎︎࣪    I N F O R M A T I O N    ❤︎︎࣪    				 
+   ❤︎︎࣪ Name: 22-5p.c
+   ❤︎︎࣪ Purpose: 
+   ❤︎︎࣪ Author: amai-aijou
+   ❤︎︎࣪ Date: Sun Sep  6 04:26:45 PM CDT 2026
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+// Modify xor.c so the input and output files are arguments.
+//  open both files in binary mode, and remove the test that checks
+//  whether the chars are printing characters
+                                                                
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                      ❤︎︎࣪    G L O B A L    ❤︎︎࣪     
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define KEY '&'
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                  ❤︎︎࣪    P R O T O T Y P E S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                ❤︎︎    M A I N  F U N C T I O N    ❤︎︎                
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+int main(int argc, char *argv[]) {
+
+	FILE *fp, *fp2;
+
+	int orig_char, new_char;
+
+	if ((fp = fopen(argv[1], "r+b")) == NULL) {
+		printf("ERROR: Could not open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((fp2 = fopen(argv[2], "r+b")) == NULL) {
+		printf("ERROR: Could not open file %s\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((orig_char = fgetc(fp)) != EOF) {
+		new_char = orig_char ^ KEY;
+		fputc(new_char,fp2);
+	}
+
+	return 0;
+}
+
+/*┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+                   ❤︎︎࣪    F U N C T I O N S    ❤︎︎࣪    
+  ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛*/
+
